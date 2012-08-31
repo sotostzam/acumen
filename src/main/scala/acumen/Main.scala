1db77721284d6525059f56c0a898bf2230e3ddec
Adam Duracz <adam@duracz.net>
2012-09-01 01:13:37 +0200

    Added choice of semantics to command line interface.
    The way the CLI is invoked has also changed. Now the first two
    parameters are the optional --semantics <interpreter-name>.
    This is followed by the path to the Acumen source file and
    finally an action that the user wants Acumen to perform.
    
    Acumen program is the first CLI parameter.

--- src/main/scala/acumen/Main.scala
+++ src/main/scala/acumen/Main.scala
@@ -16,25 +16,32 @@ import com.sun.j3d.utils.applet.MainFrame
 import javax.swing._
 
 object Main {
-    
-  def main(args : Array[String]) : Unit = {
 
-    val I = interpreters.reference.Interpreter
-    //val I = new interpreters.parallel.Interpreter(2)
-
-    val in = new InputStreamReader(System.in)
-    
-  	lazy val ast        = Parser.run(Parser.prog, in)
-    lazy val desugared  = Desugarer.run(ast)
-    lazy val dva_out    = DVA.run(desugared)
-    lazy val bta_out    = BTA.run(dva_out)
-    lazy val spec_out   = Specializer.run(bta_out)
-    lazy val nodiff_out = AD.run(spec_out)
-    lazy val trace      = I.run(nodiff_out)
-    lazy val ctrace     = trace map I.repr
+  def main(args: Array[String]): Unit = {
 
     try {
-      args(0) match {
+      /* See if user wants to choose a specific interpreter. */
+      val (i, firstNonSemanticsArg) = args(0) match {
+        case "--semantics" => args(1) match {
+          case "reference" => (interpreters.reference.Interpreter, 2)
+          case "parallel" => (new interpreters.parallel.Interpreter(2), 2)
+          case "enclosure" => (interpreters.enclosure.Interpreter, 2)
+          case _ => (interpreters.reference.Interpreter, 0)
+        }
+        case _ => (interpreters.reference.Interpreter, 0)
+      }
+      /* Read the Acumen source, parse, pre-process and interpret it. */
+      val in = new InputStreamReader(new FileInputStream(args(firstNonSemanticsArg)))
+      lazy val ast = Parser.run(Parser.prog, in)
+      lazy val desugared = Desugarer.run(ast)
+      lazy val dva_out = DVA.run(desugared)
+      lazy val bta_out = BTA.run(dva_out)
+      lazy val spec_out = Specializer.run(bta_out)
+      lazy val nodiff_out = AD.run(spec_out)
+      lazy val trace = i.run(nodiff_out)
+      lazy val ctrace = trace map i.repr
+      /* Perform user-selected action. */
+      args(firstNonSemanticsArg + 1) match {
         case "pretty" => println(pprint(ast))
         case "desugar" => println(pprint(desugared))
         case "3d" => toPython3D(toSummary3D(ctrace))
@@ -42,37 +49,37 @@ object Main {
         case "java2d" => new MainFrame(new Java3D(addThirdDimension(ctrace)), 256, 256);
         case "java3d" => new MainFrame(new Java3D(ctrace), 256, 256);
         case "json" => for (st <- ctrace) println(JSon.toJSON(st))
-        case "last" => 
-          println(pprint(prettyStore(I.repr(trace.last))))
-        case "bench" => 
-          val start : Int = Integer.parseInt(args(1))
-          val stop  : Int = Integer.parseInt(args(2))
+        case "last" =>
+          println(pprint(prettyStore(i.repr(trace.last))))
+        case "bench" =>
+          val start: Int = Integer.parseInt(args(1))
+          val stop: Int = Integer.parseInt(args(2))
           val forced = nodiff_out
           for (nbThreads <- start to stop) {
             print(nbThreads + " threads: ")
             withInterpreter(nbThreads) { PI =>
-               PI.run(forced).last 
-               print(".")
-               val startTime = System.currentTimeMillis()
-               for (_ <- 0 until 10) { print("."); PI.run(forced).last }
-               val endTime = System.currentTimeMillis()
-               println(endTime - startTime)
+              PI.run(forced).last
+              print(".")
+              val startTime = System.currentTimeMillis()
+              for (_ <- 0 until 10) { print("."); PI.run(forced).last }
+              val endTime = System.currentTimeMillis()
+              println(endTime - startTime)
             }
           }
         case "trace" =>
           var i = 0
           for (st <- ctrace) {
             println(pprint(prettyStore(st)))
-            println("-"*30 + i)
+            println("-" * 30 + i)
             i += 1
           }
-        case _ => 
+        case _ =>
           throw BadProgramOptions(
             List("pretty", "desugar", "3d", "2d", "java2d",
-                 "java3d", "json", "last", "bench from to", "trace"))
+              "java3d", "json", "last", "bench from to", "trace"))
       }
     } catch {
-      case e:AcumenError => 
+      case e: AcumenError =>
         System.err.println(e.getMessage)
         System.exit(1)
       case e => throw e
