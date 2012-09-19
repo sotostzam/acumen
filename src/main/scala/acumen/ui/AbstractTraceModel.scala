2f93b258fb099e0eef930881234eca4f81e10da3
Jan Duracz <jan@duracz.net>
2012-09-19 14:51:04 +0200

    Added Enclosure class to store plottable data for an enclosure.

--- src/main/scala/acumen/ui/AbstractTraceModel.scala
+++ src/main/scala/acumen/ui/AbstractTraceModel.scala
@@ -26,13 +26,20 @@ class PlotIntervals(simulator: Boolean, fn: Name, startFrame: Int,
   override def values : IndexedSeq[Interval] = v;
 }
 
+case class Enclosure(loLeft:Double, hiLeft:Double, loRight:Double, hiRight:Double)
+class PlotEnclosure(simulator: Boolean, fn: Name, startFrame: Int, 
+                    val v: IndexedSeq[Enclosure]) extends Plottable(simulator,fn,startFrame)
+{
+  override def values : IndexedSeq[Enclosure] = v;
+}
+
 trait AbstractTraceModel extends AbstractTableModel {
 
-  def getDouble(row:Int, column:Int) : Option[Double]
+  def getDouble(row:Int, column:Int): Option[Double]
 
-  def isEmpty() : Boolean;
+  def isEmpty(): Boolean;
 
-  def getTimes() : ArrayBuffer[Double]
-  
-  def getPlottables() : Iterable[Plottable]
+  def getTimes(): ArrayBuffer[Double]
+
+  def getPlottables(): Iterable[Plottable]
 }
