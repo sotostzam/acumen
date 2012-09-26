eb9c2b89fe7ddeb5f74e9a72e826867b7be34f54
Kevin Atkinson <k@kevina.org>
2012-09-26 15:57:52 +0200

    Avoid recomputing values in EnclosureTraceModel.

--- src/main/scala/acumen/ui/EnclosureTraceModel.scala
+++ src/main/scala/acumen/ui/EnclosureTraceModel.scala
@@ -10,33 +10,27 @@ import acumen.interpreters.enclosure.UnivariateAffineScalarEnclosure
 
 class EnclosureTraceModel(es: Seq[UnivariateAffineEnclosure]) extends AbstractTraceModel {
 
+  lazy val times = es.map(_.domain.hiDouble).foldLeft(ArrayBuffer(es.head.domain.loDouble)) { case (res, t) => res += t }
+
+  lazy val enclSeqs = es.head.varNames.zipWithIndex.map{case (name,idx) => (name, idx, null +: es.map(_(name).toEnclosure))}
+
+  lazy val plottables = enclSeqs.map {
+    case (name, idx, enclosures) =>
+      new PlotEnclosure(false, Name(name, 0), 0, idx, enclosures.toIndexedSeq)
+  }
+
   override def getRowCount() = es.size + 1
   override def getColumnCount() = es.head.varNames.size
 
-  override def getValueAt(row: Int, column: Int) = "" 
-//  {
-//    getDouble(row, column) match {
-//      case Some(v) => "[%f,%f]".format(v * 0.90, v * 1.10)
-//      case None => tm.getValueAt(row, column)
-//    }
-//  }
+  override def getValueAt(row: Int, column: Int) = ""
 
   override def getDouble(row: Int, column: Int) = Some(0) 
 
-  override def getColumnName(col: Int) = es.head.varNames.toIndexedSeq(col) 
+  override def getColumnName(col: Int) = es.head.varNames.toIndexedSeq(col)
 
   override def isEmpty() = es.isEmpty
 
-  override def getTimes() =
-    es.map(_.domain.hiDouble).foldLeft(ArrayBuffer(es.head.domain.loDouble)) { case (res, t) => res += t }
+  override def getTimes() = times
 
-  override def getPlottables() = {
-    // FIXME horrible solution, fix a.s.a.p.
-    val enclSeqs = es.head.varNames.zipWithIndex.map{case (name,idx) => (name, idx, null +: es.map(_(name).toEnclosure))}
-    enclSeqs.map {
-      case (name, idx, enclosures) =>
-        new PlotEnclosure(false, Name(name, 0), 0, idx, enclosures.toIndexedSeq)
-    }
-  }
-
+  override def getPlottables() = plottables
 }
