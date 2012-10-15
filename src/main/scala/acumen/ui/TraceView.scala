5b818002e237006cb9fdb2bba3563e8d3cb23776
Kevin Atkinson <k@kevina.org>
2012-10-15 23:00:59 +0200

    Refactor pure plotting code (i.e. with no U.I component) into its own class.

--- src/main/scala/acumen/ui/TraceView.scala
+++ src/main/scala/acumen/ui/TraceView.scala
@@ -48,7 +48,7 @@ class TraceView(
     /* for some reason forwarding events causes stack overflows, so we pass
        this to Plotter, which will ask this to publish events ... */
     private val plotter = 
-      new Plotter(plotSimulator, plotNextChild, plotSeeds, tmodel, this)
+      new PlotterPanel(plotSimulator, plotNextChild, plotSeeds, tmodel, this)
 
     val resetZoom = new Action("Reset Zoom") {
       icon = Icons.home
@@ -162,191 +162,31 @@ class TraceView(
     def setPlotStyle(ps:PlotStyle) = plotter.setPlotStyle(ps)
 }
 
-sealed trait PlotEntity { // FIXME: BetterName
-  def x1 : Double
-  def y1 : Double
-  def x2 : Double
-  def y2 : Double
-  def width = x2 - x1
-  def height = y2 - y1
-  def draw(g:Graphics2D, tr:AffineTransform);
-  def drawDots(g:Graphics2D, tr:AffineTransform);
-  def transform(tr:AffineTransform);
-}
-
-class MyPath2D() extends PlotEntity {
-  private var p1   : Point2D.Double = null
-  private var p2   : Point2D.Double = null
-  private var path : ArrayBuffer[Point2D.Double] = null
-
-  def x1 = p1.getX
-  def y1 = p1.getY
-  def x2 = p2.getX
-  def y2 = p2.getY
-
-  private def update(x:Double, y:Double) = {
-    val minX = math.min(x1, x)
-    val maxX = math.max(x2, x)
-    val minY = math.min(y1, y)
-    val maxY = math.max(y2, y)
-    p1 = new Point2D.Double(minX, minY)
-    p2 = new Point2D.Double(maxX, maxY)
-  }
-  def startAt(x:Double, y:Double) = {
-    p1 = new Point2D.Double(x,0)
-    p2 = new Point2D.Double(x,0)
-    update(x,y)
-    path = ArrayBuffer(new Point2D.Double(x,y))
-  }
-  def goTo(x:Double, y:Double) = {
-    path += new Point2D.Double(x,y)
-    update(x,y)
-  }
-  def draw(g:Graphics2D, tr:AffineTransform) = {
-    if (path.size >= 2) {
-      for (i <- 0 until (path.size-1)) {
-        val p0 = new Point2D.Double()
-        val p1 = new Point2D.Double()
-        tr.transform(path(i), p0)
-        tr.transform(path(i+1), p1)
-        g.draw(new Line2D.Double(p0, p1))
-      }
-    }
-  }
-  def drawDots(g:Graphics2D, tr:AffineTransform) = {
-    if (path.size >= 2) {
-      for (i <- 0 until path.size) {
-        val p = new Point2D.Double()
-        tr.transform(path(i), p)
-        g.fillRect(p.getX.toInt, p.getY.toInt, 2, 2)
-      }
-    }
-  }
-  def transform(tr:AffineTransform) = {
-    for (p <- path) tr.transform(p, p)
-    tr.transform(p1, p1)
-    tr.transform(p2, p2)
-    val minX = math.min(x1, x2)
-    val maxX = math.max(x1, x2)
-    val minY = math.min(y1, y2)
-    val maxY = math.max(y1, y2)
-    p1 = new Point2D.Double(minX, minY)
-    p2 = new Point2D.Double(maxX, maxY)
-  }
-}
-
-class EnclosurePath() extends PlotEntity {
-  private var p1  = new Point2D.Double(0,0)
-  private var p2  = new Point2D.Double(0,0)
-  case class PolyPoints(a:Point2D.Double, b:Point2D.Double,
-                        c:Point2D.Double, d:Point2D.Double) 
-  {
-    def transform(tr:AffineTransform, res: PolyPoints) = {
-      tr.transform(a,res.a)
-      tr.transform(b,res.b)
-      tr.transform(c,res.c)
-      tr.transform(d,res.d)
-    }
-  }
-  private var polys = new ArrayBuffer[PolyPoints]
-
-  def x1 = p1.getX
-  def y1 = p1.getY
-  def x2 = p2.getX
-  def y2 = p2.getY
-
-  private def update(x:Double, y:Double) = {
-    val minX = math.min(x1, x)
-    val maxX = math.max(x2, x)
-    val minY = math.min(y1, y)
-    val maxY = math.max(y2, y)
-    p1 = new Point2D.Double(minX, minY)
-    p2 = new Point2D.Double(maxX, maxY)
-  }
-  def add(x0: Double, x1:Double, ys: Enclosure) {
-    polys += PolyPoints(new Point2D.Double(x0,ys.hiLeft),
-                        new Point2D.Double(x1,ys.hiRight),
-                        new Point2D.Double(x1,ys.loRight),
-                        new Point2D.Double(x0,ys.loLeft))
-    update(x0,ys.hiLeft);
-    update(x1,ys.hiRight);
-    update(x1,ys.loRight);
-    update(x0,ys.loLeft);
-  }
-  def draw(g:Graphics2D, tr:AffineTransform) = {
-    val area = new Area();
-    val lines = new ArrayBuffer[Line2D.Double];
-    g.setStroke(new BasicStroke(1))
-    for (polyPoints <- polys) {
-      val toDraw = PolyPoints(new Point2D.Double,
-                              new Point2D.Double,
-                              new Point2D.Double,
-                              new Point2D.Double);
-      polyPoints.transform(tr, toDraw)
-      val polyPath = new Path2D.Double;
-      polyPath.moveTo(toDraw.a.getX(),toDraw.a.getY())
-      polyPath.lineTo(toDraw.b.getX(),toDraw.b.getY())
-      polyPath.lineTo(toDraw.c.getX(),toDraw.c.getY())
-      polyPath.lineTo(toDraw.d.getX(),toDraw.d.getY())
-      polyPath.closePath()
-      area.add(new Area(polyPath))
-      // Draw exact solutions as a line
-      if (toDraw.a == toDraw.d && toDraw.b == toDraw.c)
-        g.draw(new Line2D.Double(toDraw.a.getX(),toDraw.a.getY(),toDraw.b.getX(),toDraw.b.getY()))
-    }
-    g.draw(area)
-    val prevComposite = g.getComposite()
-    g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.10f))
-    g.fill(area)
-    g.setComposite(prevComposite)
-  }
-
-  def drawDots(g:Graphics2D, tr:AffineTransform) = draw(g,tr)
-
-  def transform(tr:AffineTransform) = {
-    for (p <- polys) p.transform(tr,p)
-    tr.transform(p1, p1)
-    tr.transform(p2, p2)
-    val minX = math.min(x1, x2)
-    val maxX = math.max(x1, x2)
-    val minY = math.min(y1, y2)
-    val maxY = math.max(y1, y2)
-    p1 = new Point2D.Double(minX, minY)
-    p2 = new Point2D.Double(maxX, maxY)
-  }
-}
-
 case class PointedAtEvent(time:Double, name:String, value:String) extends Event
 
-class Plotter(
+class PlotterPanel(
   _plotSimulator: Boolean, _plotNextChild:Boolean, 
 	_plotSeeds:Boolean, tb:AbstractTraceModel, pub:Publisher) extends Panel {
 
   background = Color.gray
   peer.setDoubleBuffered(true)
+
+  private var pd = new PlotData(false,false,false,null)
  
   private val selectionBorderColor = new Color(68, 127, 231)
   private val selectionFillColor   = new Color(165, 189, 231, 150)
  
   private var buffer : BufferedImage = null
-  private var polys = new ArrayBuffer[PlotEntity]()
-  private var axes  = new ArrayBuffer[MyPath2D]()
-  private var boxes = new ArrayBuffer[Rectangle2D]()
   private var hoveredBox : Option[Int] = None
   private var dotX : Double = 0.0d
   private var dotY : Option[Double] = None
   private var selection : Option[(Point2D, Point2D)] = None
   private var drag : Option[(Point2D,Point2D)] = None
-  private var boundingBox = (0.0, 0.0)
 
   private var viewPort : Rectangle2D = new Rectangle2D.Double(0,0,0,0)
   private val undoStack = new Stack[Rectangle2D] 
   private var viewChanged = true
   private var transform = new AffineTransform()
-  private var time : IndexedSeq[Double] = new ArrayBuffer[Double]
-  private var traceViewTime : IndexedSeq[Double]= new ArrayBuffer[Double]
-  private var columnIndices = new ArrayBuffer[Int]
-  private var yTransformations = new ArrayBuffer[(Double, Double)]
 
   private var plotNextChild = _plotNextChild
   private var plotSimulator = _plotSimulator
@@ -354,24 +194,18 @@ class Plotter(
   private var plotStyle : PlotStyle = Lines()
 
   def clear = {
+    pd = new PlotData(false,false,false,null)
     buffer = null
-    polys = new ArrayBuffer[PlotEntity]()
-    axes  = new ArrayBuffer[MyPath2D]()
-    boxes = new ArrayBuffer[Rectangle2D]()
     hoveredBox = None
     dotX = 0.0d
     dotY = None
     selection = None
     drag = None
-    boundingBox = (0.0, 0.0)
+    pd.boundingBox = (0.0, 0.0)
     viewPort = new Rectangle2D.Double(0,0,0,0)
     undoStack.clear 
     viewChanged = true
     transform = new AffineTransform()
-    time = new ArrayBuffer[Double]
-    traceViewTime = new ArrayBuffer[Double]
-    columnIndices = new ArrayBuffer[Int]
-    yTransformations = new ArrayBuffer[(Double, Double)]
     repaint
   }
 
@@ -432,7 +266,7 @@ class Plotter(
   def fit : Unit = {
     if (buffer != null) {
       undoStack.clear
-      val bb = new Rectangle2D.Double(0, 0, boundingBox._1, boundingBox._2)
+      val bb = new Rectangle2D.Double(0, 0, pd.boundingBox._1, pd.boundingBox._2)
       val tr = computeTransform(buffer.getWidth, buffer.getHeight, bb)
       val trbb = applyTr(tr, bb)
       trbb.setRect(trbb.getX-10, trbb.getY-10, trbb.getWidth+20, trbb.getHeight+20)
@@ -480,7 +314,7 @@ class Plotter(
     bg.setClip(new Rectangle2D.Double(0, 0, buf.getWidth, buf.getHeight))
 
     /* actual drawing */
-    for (((p,a),b) <- polys zip axes zip boxes) {
+    for (((p,a),b) <- pd.polys zip pd.axes zip pd.boxes) {
 
       // fill bounding box
       bg.setPaint(Color.white)
@@ -509,12 +343,14 @@ class Plotter(
   }
 
   override def paintComponent(g: Graphics2D) {
+
     g.setRenderingHint(
       RenderingHints.KEY_ANTIALIASING,
       RenderingHints.VALUE_ANTIALIAS_ON)
     super.paintComponent(g)
 
     if (viewChanged) {
+      println("View Changed Man!!")
       /* create a new buffer for the background */
       buffer = mkBuffer
       /* compute the current transformation */
@@ -540,7 +376,7 @@ class Plotter(
     g.setStroke(new BasicStroke(2.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND))
     hoveredBox match {
       case Some(hb) =>
-        val rec = applyTr(boxes(hb))
+        val rec = applyTr(pd.boxes(hb))
         val recPlusOne = 
           new Rectangle2D.Double(
             rec.getX-2, rec.getY-2, 
@@ -576,103 +412,26 @@ class Plotter(
     }
   }
 
-  private def plotit(p: Plottable) = {
-    (plotSimulator || !p.simulator) && 
-    (plotNextChild || p.fn != Name("nextChild",0)) &&
-		(plotSeeds || (p.fn != Name("seed1",0) && p.fn !=  Name("seed2",0)))
-  }
-
-  def redraw = {
-  
-    polys = new ArrayBuffer[PlotEntity]
-    axes  = new ArrayBuffer[MyPath2D]
-    boxes = new ArrayBuffer[Rectangle2D]
-    yTransformations = new ArrayBuffer[(Double,Double)]
-    
-    if (tb.isEmpty) ()
-    else {
-
-      time = tb.getTimes()
-      traceViewTime = tb.getTraceViewTimes()
-
-      columnIndices = new ArrayBuffer[Int]
-      for (p <- tb.getPlottables() if plotit(p)) {
-        var s = p.startFrame
-        columnIndices += p.column
-
-        val ax = new MyPath2D()
-        ax startAt (time(s), 0)
-        ax goTo (time(math.min(s+p.values.size, time.size-1)), 0)
-        axes += ax
-
-        p match {
-          case p:PlotDoubles => {
-            val line = new MyPath2D();
-            line startAt (time(s), p.values(0))
-            
-            for (f <- 0 until p.values.size;
-                 val frame = s + f) {
-              line goTo (time(frame), p.values(f))
-            }
-
-            polys += line
-          }
-          case p:PlotEnclosure => {
-            val path = new EnclosurePath
-            var prevTime = time(s)
-            for (f <- 1 until p.values.size;
-                 val frame = s + f) {
-              if (time(frame-1) != time(frame))
-                  prevTime = time(frame-1)
-              path.add(prevTime, time(frame), p.values(f))
-            }
-            polys += path
-          }
-        }
-      }
-      //normalize (scale)
-      for (((p,a),i) <- polys zip axes zip Stream.from(0)) {
-        var scale = 1.0
-        if (p.height > 1e-4) {
-          scale = -1.0 / p.height
-          val tr1 = AffineTransform.getScaleInstance(1.0, scale)
-          p.transform(tr1)
-          a.transform(tr1)
-        }
-        val shift = i*1.2 - p.y1
-        val tr2 = AffineTransform.getTranslateInstance(0.0, shift)
-        p.transform(tr2)
-        a.transform(tr2)
-
-        yTransformations += ((scale, shift))
-        boxes += new Rectangle2D.Double(p.x1, i*1.2, p.width, 1.0)
-      }
-      boundingBox = (time(time.size-1), 1.2*polys.size - 0.2)
-    }
-    viewChanged = true
-    repaint
-  }
-
   /* mouse handling */
 
   // binary search
   private def findTimeIndex(t:Double) : (Double, Int) = {
     def findIn(min:Int, max:Int) : (Double, Int) = {
       if (max - min <= 1) {
-        val tmax = traceViewTime(max)
-        val tmin = traceViewTime(min)
+        val tmax = pd.traceViewTime(max)
+        val tmin = pd.traceViewTime(min)
         if (math.abs(tmax-t) < math.abs(tmin-t)) 
           (tmax, max)
         else (tmin, min)
       }
       else {
         val middle = (max+min)/2
-        if (traceViewTime(middle) < t) findIn(middle, max)
+        if (pd.traceViewTime(middle) < t) findIn(middle, max)
         else findIn(min, middle)
       }
     }
-    if (traceViewTime.isEmpty) (0,0)
-    else findIn(0, traceViewTime.size-1)
+    if (pd.traceViewTime.isEmpty) (0,0)
+    else findIn(0, pd.traceViewTime.size-1)
   }
 
   listenTo(mouse.moves)
@@ -682,7 +441,7 @@ class Plotter(
 
   reactions += {
     case MouseMoved(_, p, _) =>
-      if (columnIndices.size > 0) {
+      if (pd.columnIndices.size > 0) {
         // p in the original coordinate system
         val op = unapplyTr(p)
         // quantized time and row in the trace model corresponding to p.x
@@ -692,9 +451,9 @@ class Plotter(
 
         // index of the curve pointed by the mouse
         val hb = (op.getY / 1.2).toInt
-        if (hb < columnIndices.length&&hb>=0) {
+        if (hb < pd.columnIndices.length&&hb>=0) {
           // corresponding column in the trace model
-          val column = columnIndices(hb)
+          val column = pd.columnIndices(hb)
           // name of the column
           val name = tb.getColumnName(column)
           // value of the column as a string
@@ -715,7 +474,7 @@ class Plotter(
           // publish a pointed at event
           pub.publish(PointedAtEvent(qt, name, value))
           // update the green dot Y coordinate
-          val (scale, shift) = yTransformations(hb)
+          val (scale, shift) = pd.yTransformations(hb)
           dotY = for (y <- tb.getDouble(row, column))
                    yield applyTr(new Point2D.Double(0, y*scale + shift)).getY
           // updating the hovered box number
@@ -807,6 +566,13 @@ class Plotter(
 
   def zoom(factor:Double) = 
     zoomAround(viewPort.getCenterX, viewPort.getCenterY, factor)
+
+  def redraw = {
+    println("Redraw man!")
+    pd = new PlotData(plotSimulator,plotNextChild,plotSeeds,tb)
+    viewChanged = true
+    repaint
+  }
 
   def toggleSimulator(b:Boolean) = { 
     plotSimulator = b
