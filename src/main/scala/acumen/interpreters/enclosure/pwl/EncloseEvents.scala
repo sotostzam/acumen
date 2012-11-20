4f23b12ad0114cac8345196e0c79c237c434880e
Jan Duracz <jan@duracz.net>
2012-11-20 19:31:20 +0100

    Finished first implementation of enclose-events (a.k.a. PWL).
    This is an alternative algorithm for enclosing a system over fixed
    time segments. It does not explicitly construct an event tree, but
    instead collapses each level in the implicit tree into the box it
    will eventually return for each mode.
    
    The algorithm is a drop-in preplacement for solve-VtE.

--- src/main/scala/acumen/interpreters/enclosure/pwl/EncloseEvents.scala
+++ src/main/scala/acumen/interpreters/enclosure/pwl/EncloseEvents.scala
@@ -8,15 +8,72 @@ import acumen.interpreters.enclosure.Box
 import acumen.interpreters.enclosure.Interval
 import acumen.interpreters.enclosure.Rounding
 import acumen.interpreters.enclosure.UnivariateAffineEnclosure
+import acumen.interpreters.enclosure.tree.SolveIVP
+import acumen.interpreters.enclosure.Parameters
 
-class EncloseEvents(implicit rnd: Rounding) extends App {
+trait EncloseEvents extends SolveIVP {
+  
+  // main function
+
+  def encloseEvents(ps: Parameters, h: HybridSystem, t: Interval, s: StateEnclosure): (StateEnclosure, StateEnclosure) = {
+    implicit val rnd = ps.rnd
+    val (s0, fin0) = encloseFlowNoEvents(ps, h, s, t)
+    val sEvents = reachableStatesPWL(ps, h, t, emptyState(h), s0)
+    (union(Set(s0, sEvents)), union(Set(fin0, sEvents)))
+  }
+
+  // helper functions
+
+  def reachableStatesPWL(ps: Parameters, h: HybridSystem, t: Interval, pIn: StateEnclosure, wIn: StateEnclosure): StateEnclosure = {
+    implicit val rnd = ps.rnd
+    if (isDefinitelyEmpty(wIn)) pIn
+    else {
+      val (sEvolved, _) = encloseFlowNoEvents(ps, h, encloseOneEvent(h, wIn), t)
+      val w = minus(sEvolved, pIn)
+      val p = union(Set(pIn, sEvolved))
+      reachableStatesPWL(ps, h, t, p, w)
+    }
+  }
+
+  def encloseOneEvent(h: HybridSystem, s: StateEnclosure)(implicit rnd: Rounding): StateEnclosure =
+    union(h.events.map(e => reset(h, e, intersectGuard(h, e, s))))
+
+  def encloseFlowNoEvents(ps: Parameters, h: HybridSystem, sIn: StateEnclosure, t: Interval): (StateEnclosure, StateEnclosure) = {
+    implicit val rnd = ps.rnd
+    val sPreAndFinalPre = sIn map {
+      case (q, None) => (q, (None, None))
+      case (q, Some(box)) => (q, encloseFlowRange(ps, h.fields(q), t, box))
+    }
+    val s = intersectInv(h, sPreAndFinalPre.mapValues(_._1))
+    val fin = intersectInv(h, sPreAndFinalPre.mapValues(_._2))
+    (s, fin)
+  }
+
+  def encloseFlowRange(ps: Parameters, f: Field, t: Interval, b: Box): (OBox, OBox) = {
+    val flow = encloseFlow(ps, f, t, b)
+    (Some(flow(t)), Some(flow(t.high)))
+  }
+
+  def encloseFlow(ps: Parameters, f: Field, t: Interval, b: Box): UnivariateAffineEnclosure = {
+    implicit val rnd = ps.rnd
+    solveVt(f, t, b,
+      ps.solveVtInitialConditionPadding,
+      ps.extraPicardIterations,
+      ps.maxPicardIterations,
+      ps.splittingDegree,
+      "output")
+  }
+
+  // types for representing enclosures
 
   type OBox = Option[Box]
 
   type StateEnclosure = Map[Mode, OBox]
 
+  // operations on enclosures
+
   /** mode-wise union */
-  def union(ss: Set[StateEnclosure]): StateEnclosure = {
+  def union(ss: Set[StateEnclosure])(implicit rnd: Rounding): StateEnclosure = {
     def hull(obs: Seq[OBox]): OBox = {
       def ohull(l: OBox, r: OBox) =
         if (l isEmpty) r
@@ -34,7 +91,7 @@ class EncloseEvents(implicit rnd: Rounding) extends App {
   }
 
   /** mode-wise set difference */
-  def minus(l: StateEnclosure, r: StateEnclosure): StateEnclosure = {
+  def minus(l: StateEnclosure, r: StateEnclosure)(implicit rnd: Rounding): StateEnclosure = {
     require(l.keySet == r.keySet)
     def ominus(l: OBox, r: OBox): OBox =
       if (l.isEmpty || r.isEmpty) l
@@ -44,7 +101,7 @@ class EncloseEvents(implicit rnd: Rounding) extends App {
 
   /** mode-wise intersection with invariant */
   // TODO solve this without try-catch
-  def intersectInv(h: HybridSystem, s: StateEnclosure): StateEnclosure =
+  def intersectInv(h: HybridSystem, s: StateEnclosure)(implicit rnd: Rounding): StateEnclosure =
     s.map {
       case (mode, obox) => mode ->
         (try {
@@ -54,7 +111,7 @@ class EncloseEvents(implicit rnd: Rounding) extends App {
 
   /** mode-wise intersection with the guard of `e` */
   // TODO solve this without try-catch
-  def intersectGuard(h: HybridSystem, e: Event, s: StateEnclosure): StateEnclosure =
+  def intersectGuard(h: HybridSystem, e: Event, s: StateEnclosure)(implicit rnd: Rounding): StateEnclosure =
     s.mapValues(
       obox => try {
         Some(h.guards(e).support(obox.get))
@@ -62,52 +119,17 @@ class EncloseEvents(implicit rnd: Rounding) extends App {
 
   /** mode-wise application of reset for `e` */
   // TODO solve this without try-catch
-  def reset(h: HybridSystem, e: Event, s: StateEnclosure): StateEnclosure =
+  def reset(h: HybridSystem, e: Event, s: StateEnclosure)(implicit rnd: Rounding): StateEnclosure =
     s.mapValues(
       obox => try {
         Some(h.resets(e)(obox.get))
       } catch { case _ => None })
 
   /** the state enclosure that is empty in each mode */
-  def emptyState(H: HybridSystem): StateEnclosure = H.modes.map(_ -> None).toMap
+  def emptyState(h: HybridSystem): StateEnclosure = h.modes.map(_ -> None).toMap
 
   /** check that `s` is empty for each mode */
   def isDefinitelyEmpty(s: StateEnclosure): Boolean =
     s.forall(_._2 isEmpty)
-
-  def encloseEvents(h: HybridSystem, t: Interval, s: StateEnclosure): (StateEnclosure, StateEnclosure) = {
-    val (s0, fin0) = encloseFlowNoEvents(h, s, t)
-    val sEvents = reachableStatesPWL(h, t, emptyState(h), s0)
-    (union(Set(s0, sEvents)), union(Set(fin0, sEvents)))
-  }
-
-  def reachableStatesPWL(h: HybridSystem, t: Interval, pIn: StateEnclosure, wIn: StateEnclosure): StateEnclosure =
-    if (isDefinitelyEmpty(wIn)) pIn
-    else {
-      val (sEvolved, _) = encloseFlowNoEvents(h, encloseOneEvent(h, wIn), t)
-      val w = minus(sEvolved, pIn)
-      val p = union(Set(pIn, sEvolved))
-      reachableStatesPWL(h, t, p, w)
-    }
-
-  def encloseOneEvent(h: HybridSystem, s: StateEnclosure): StateEnclosure =
-    union(h.events.map(e => reset(h, e, intersectGuard(h, e, s))))
-
-  def encloseFlowNoEvents(h: HybridSystem, sIn: StateEnclosure, t: Interval): (StateEnclosure, StateEnclosure) = {
-    val sPreAndFinalPre = sIn map {
-      case (q, None) => (q, (None, None))
-      case (q, Some(box)) => (q, encloseFlowRange(h.fields(q), t, box))
-    }
-    val s = intersectInv(h, sPreAndFinalPre.mapValues(_._1))
-    val fin = intersectInv(h, sPreAndFinalPre.mapValues(_._2))
-    (s, fin)
-  }
-
-  def encloseFlowRange(f: Field, t: Interval, b: Box): (OBox, OBox) = {
-    val flow = encloseFlow(f, t, b)
-    (Some(flow(t)), Some(flow(t.high)))
-  }
-
-  def encloseFlow(f: Field, t: Interval, b: Box): UnivariateAffineEnclosure = null
 
 }
