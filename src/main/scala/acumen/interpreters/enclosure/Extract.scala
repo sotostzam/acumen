1523b3fb7e41b7490e5a8b4e2c29715634d07af2
Jan Duracz <jan@duracz.net>
2012-11-20 19:30:43 +0100

    Factored out solver parametrers from Extract to separate class.

--- src/main/scala/acumen/interpreters/enclosure/Extract.scala
+++ src/main/scala/acumen/interpreters/enclosure/Extract.scala
@@ -73,24 +73,6 @@ trait Extract {
       }
     }
 
-  /** Parameters for solve-hybrid */
-  case class Parameters(
-    precision: Int,
-    startTime: Double, // simulation start time
-    endTime: Double, // simulation end time
-    solveVtInitialConditionPadding: Double, // padding for initial condition in solveVt
-    extraPicardIterations: Int, // number of extra Picard iterations in solveVt
-    maxPicardIterations: Int, // maximum number of Picard iterations in solveVt
-    maxEventTreeSize: Int, // maximum event tree size in solveVtE, gives termination condition for tree enlargement
-    minTimeStep: Double, // minimum time step size
-    maxTimeStep: Double, // maximum time step size
-    minImprovement: Double, // minimum improvement of enclosure
-    splittingDegree: Int // number of pieces to split each initial condition variable 
-    ) {
-    implicit val rnd = Rounding(precision)
-    val simulationTime = Interval(startTime, endTime)
-  }
-
   /**
    * Extracts solver parameter values embedded in an Acumen class
    *
