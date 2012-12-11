26e74e0486e39d0f93111f690171862347541571
Kevin Atkinson <k@kevina.org>
2012-12-11 03:00:09 +0100

    Create example shell scripts for using the enclosure benchmarks.

---
+++ benchmark/one-parm.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+#
+# Example script to run enclosure benchmarks varying one parameter
+#
+# Run this example in the root of the source tree, not in the
+# benchmark directory.  Its usage will be:
+#   sh benchmark/one-parm.sh
+# the results by default will go in a new dir: benchmark-res
+#
+# The main products will be two graphs
+#   $prefix-runtime.png
+#   $prefix-precision.png
+# You will also notice lots of pdf files generated in the form of
+#   $prefix-###.pdf
+# these are the actual results of running the model with the
+# paramter set to ###
+# A bunch of raw data files are also generated
+#
+# Hint: To tweak the graphs comment out the sbt line to avoid
+# regenerating the data 
+
+set -e
+
+title="Zeno Half Overconstrained"
+model=examples/XXX_internal/enclosure/zeno/02_Zeno_Half_Overconstrained.acm
+resultDir=benchmark-res
+prefix=Zeno_Half_Overconstrained-minImprovment
+parm=minImprovement
+values=0.00001,0.0001,0.001,0.005
+
+PREFIX="./$resultDir/$prefix"
+
+mkdir -p $resultDir
+
+sbt "run-main acumen.Main --semantics enclosure $model bench-enclosures $PREFIX $parm=$values"
+
+gnuplot <<EOF
+set terminal png
+set output "$PREFIX-runtime.png"
+set title "$title"
+set xlabel "$parm"
+set ylabel "runtime"
+unset key # don't display a legend
+set logscale x
+plot "$PREFIX-runtime.dat" using 1:3 with lines, \
+     "$PREFIX-runtime.dat" using 1:3:5 with errorbars
+EOF
+
+gnuplot <<EOF
+set terminal png
+set output "$PREFIX-precision.png"
+set title "$title"
+set xlabel "$parm"
+set ylabel "precision"
+unset key # don't display a legend
+set logscale x
+plot "$PREFIX-precision-norm.dat" using 1:3 with lines
+EOF
+
---
+++ benchmark/two-parms.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+set -e
+
+#
+# Example script to run enclosure benchmarks varying two parameter
+#
+# Like one-parm.sh but varies two and attempts to display them
+# using gnuplot, although not very well
+#
+
+title="Zeno Half Overconstrained"
+model=examples/XXX_internal/enclosure/zeno/02_Zeno_Half_Overconstrained.acm
+resultDir=benchmark-res
+prefix=Zeno_Half_Overconstrained-minImprovment,minTimeStep
+parm1=minImprovement
+values1=0.00001,0.0001,0.001,0.005
+parm2=minTimeStep
+values2=0.0001,0.001,0.01,0.1
+
+PREFIX="./$resultDir/$prefix"
+
+mkdir -p $resultDir
+
+sbt "run-main acumen.Main --semantics enclosure $model bench-enclosures $PREFIX $parm1=$values1 $parm2=$values2"
+
+gnuplot <<EOF
+set terminal png
+set output "$PREFIX-runtime.png"
+set title "$title"
+set xlabel "$parm1"
+set ylabel "$parm2"
+set zlabel "runtime"
+unset key # don't display a legend
+set logscale x
+set logscale y
+splot "$PREFIX-runtime.dat" using 1:2:4
+EOF
+
+gnuplot <<EOF
+set terminal png
+set output "$PREFIX-precision.png"
+set title "$title"
+set xlabel "$parm1"
+set ylabel "$parm2"
+set zlabel "runtime"
+unset key # don't display a legend
+set logscale x
+set logscale y
+splot "$PREFIX-precision-norm.dat" using 1:2:4
+EOF
+
