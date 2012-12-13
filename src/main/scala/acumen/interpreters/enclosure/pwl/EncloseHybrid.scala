1fdbc23bc552f55708d4a9b60468fe3b0f57b55f
Jan Duracz <jan@duracz.net>
2012-12-13 03:13:22 +0100

    Committing missed bugfix for encloseHybrid.

--- src/main/scala/acumen/interpreters/enclosure/pwl/EncloseHybrid.scala
+++ src/main/scala/acumen/interpreters/enclosure/pwl/EncloseHybrid.scala
@@ -27,7 +27,7 @@ trait EncloseHybrid extends EncloseEvents {
 
     val teL = (for ((_, mae, _) <- lfes.values if !mae.isEmpty) yield domain(mae).low).foldLeft(t.high)(Interval.min)
 
-    val teR = (for ((_, mae, compl) <- lfes.values if compl) yield domain(mae).high).foldLeft((Interval.min(t.high, teL + ps.maxTimeStepLocalisation)))(Interval.min)
+    val teR = (for ((_, mae, compl) <- lfes.values if compl) yield domain(mae).high).foldLeft((Interval.min(t.high, teL + ps.maxEventStepSize)))(Interval.min)
 
     val noe = unionOfEnclosureListsUntil(teL, lfes.values.toSeq.map { case (noe, _, _) => noe })
     if (teL equalTo t.high) { // proved that there no event at all on T
