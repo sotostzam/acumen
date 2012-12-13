3c9de9afae99fe54b2ae3863581c0745832d7e6d
Jan Duracz <jan@duracz.net>
2012-12-13 05:15:36 +0100

    Removed deprecated maxEventStepSize parameter.

--- src/main/scala/acumen/interpreters/enclosure/Extract.scala
+++ src/main/scala/acumen/interpreters/enclosure/Extract.scala
@@ -113,7 +113,6 @@ trait Extract {
           "minTimeStep" -> 0.01,
           "minTimeStepODEsolving" -> 0.01,
           "minTimeStepLocalisation" -> 0.001,
-          "maxEventStepSize" -> 0.1,
           "maxTimeStep" -> 3,
           "minImprovement" -> 0.0001,
           "splittingDegree" -> 1)
@@ -132,12 +131,6 @@ trait Extract {
             val endTime = updatedParameters("endTime")
             updatedParameters + ("maxTimeStep" -> (endTime - startTime))
           }
-          if (assignedParameters contains "maxEventStepSize")
-            updatedParameters
-          else {
-            val minTimeStepLocalisation = updatedParameters("minTimeStepLocalisation")
-            updatedParameters + ("maxEventStepSize" -> (minTimeStepLocalisation * 100))
-          }
         }
         Parameters(
           params("precision").toInt,
@@ -150,7 +143,6 @@ trait Extract {
           params("minTimeStep"),
           params("minTimeStepODEsolving"),
           params("minTimeStepLocalisation"),
-          params("maxEventStepSize"),
           params("maxTimeStep"),
           params("minImprovement"),
           params("splittingDegree").toInt)
--- src/main/scala/acumen/interpreters/enclosure/Parameters.scala
+++ src/main/scala/acumen/interpreters/enclosure/Parameters.scala
@@ -12,7 +12,6 @@ case class Parameters(
   minTimeStep: Double, // minimum time step size
   minTimeStepODEsolving: Double, // minimum time step size during ODE solving
   minTimeStepLocalisation: Double, // minimum time step size during event localization
-  maxEventStepSize: Double, // maximum time step size during event localization
   maxTimeStep: Double, // maximum time step size
   minImprovement: Double, // minimum improvement of enclosure
   splittingDegree: Int // number of pieces to split each initial condition variable 
