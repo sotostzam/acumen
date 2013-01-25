b48ce07ea687989cc91e921b1b9348e970ae4f76
Kevin Atkinson <k@kevina.org>
2013-01-25 16:34:29 +0100

    Bug Fix, UseQuartz may be null if not set on Mac OS X.

--- src/main/scala/acumen/ui/GraphicalMain.scala
+++ src/main/scala/acumen/ui/GraphicalMain.scala
@@ -35,7 +35,7 @@ object GraphicalMain extends SimpleSwingApplication {
   var threeDState = ThreeDState.LAZY
   val osname = System.getProperty("os.name").toLowerCase
   var need_quarts = false
-  if (osname == "mac os x" && System.getProperty("apple.awt.graphics.UseQuartz") == "false") {
+  if (osname == "mac os x" && !(System.getProperty("apple.awt.graphics.UseQuartz") == "true")) {
     threeDState = ThreeDState.DISABLE
     need_quarts = true
   }
