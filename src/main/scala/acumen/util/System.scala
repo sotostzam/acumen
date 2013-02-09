ea621cd026a7cf1af97d8d17ea70a876f18376e7
Jan Duracz <jan@duracz.net>
2013-02-09 01:17:23 +0100

    Fixed comment typo in acumen.util.System.

--- src/main/scala/acumen/util/System.scala
+++ src/main/scala/acumen/util/System.scala
@@ -5,7 +5,7 @@ import java.io.FileInputStream
 
 object System {
  
-  val ACUMEN_FILE_SUFFIX = ".acm"
+  val FILE_SUFFIX_MODEL = ".acm"
   
   val version = {
     val md = getClass.getClassLoader.getResource("acumen/version")
@@ -18,21 +18,21 @@ object System {
     new java.net.URI("http://www.acumen-language.org/p/tutorials.html")
   
   /** 
-   * Reads each file in the folder at path that end with extensionFilter into a String.
+   * Reads each file in the folder at path that ends with extensionFilter into a String.
    * Returns an Iterable of pairs, with the first String equal to the file's contents
    * and the second equal to the text contents of the string (read as UTF-8).
    **/
-  def readFiles(path: String, extensionFilter: String): Iterable[(String, String)] = {
+  def readFiles(path: String, extensionFilter: String): Map[String, String] = {
     new File(path).listFiles.filter(_.getName.endsWith(extensionFilter)).map { 
       f: File => {
         val n = f getName
-        val in = new FileInputStream(path + File.pathSeparator + n)
+        val in = new FileInputStream(path + File.separator + n)
         val s = scala.io.Source.fromInputStream(in).getLines().mkString("\n")
         in close
-        val prefix = n.substring(0, n.lastIndexOf(ACUMEN_FILE_SUFFIX))
+        val prefix = n.substring(0, n.lastIndexOf(extensionFilter))
         (prefix, s)
       }
-    }
+    }.toMap
   }
   
 }
