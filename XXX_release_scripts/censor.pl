3056b120bf17efdda0df7766468a30ec82dfad5c
Kevin Atkinson <k@kevina.org>
2013-03-09 21:06:05 +0100

    Make censor script more robust.

--- XXX_release_scripts/censor.pl
+++ XXX_release_scripts/censor.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 
+$/ = undef;  # see perlvar man page for meaning
+
 use File::Find;
 
 if (! -e "READY_FOR_CENSOR" ) {
@@ -11,18 +13,36 @@ if (! -e "READY_FOR_CENSOR" ) {
     exit (1);
 }
 
+$SIG{__DIE__} = sub {
+    die @_ if $^S;
+    local $_ = $_[0];
+    chop $_;
+    print "$_  Aborting.\n";
+    exit 2;
+};
+
+
 print "Removing all XXX* files\n";
-system("find . -iname 'xxx*' -print0 | xargs -0 rm -rf");
+my $find_err_string = "Problem when finding XXX Files to remove";
+my $xargs_err_string = "Problem when removing XXX Files";
+
+open F, "find . -iname 'xxx*' -print0 | " or die $find_err_string;
+my $find_res = <F>;
+close F or die $find_err_string;
+
+open F, "| xargs -0 rm -rf" or die $xargs_err_string;
+print F <F>;
+close F or die $xargs_err_string;
 
 File::Find::find(sub {
     return unless /^.*\.scala\z/si;
+    my $path = $File::Find::name;
     my $file = $_;
-    local $/ = undef;
-    open F, "$file" or die "Unable to open $file";
+    open F, "$file" or die "Unable to read $path";
     $_ = <F>;
     my $res = s/^.*\/\/ *NRL.*\n//mg;
     return unless $res > 0;
-    open F, ">$file" or die "Cannot right to $file";
+    open F, ">$file" or die "Cannot right to $path";
     print "Censoring $file\n";
     print F $_;
     close F;
