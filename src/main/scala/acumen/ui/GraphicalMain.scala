a56042ab27845cf7d2b50d5ad4f421503bc8a411
Adam Duracz <adam@duracz.net>
2013-05-22 01:02:52 +0200

    Replaced --completion CLI option with --{en,dis}able-completion.

--- src/main/scala/acumen/ui/GraphicalMain.scala
+++ src/main/scala/acumen/ui/GraphicalMain.scala
@@ -84,8 +84,10 @@ object GraphicalMain extends SimpleSwingApplication {
         useEnclosures = false; parseOpts(tail)
       case "--non-localizing" :: tail =>
         interpreters.enclosure.Interpreter.localizing = false; parseOpts(tail)
-      case "--completion" :: tail =>
+      case "--enable-completion" :: tail =>
         useCompletion = true; parseOpts(tail)
+      case "--disable-completion" :: tail =>
+        useCompletion = false; parseOpts(tail)
       case "--templates" :: tail =>
         useTemplates = true; parseOpts(tail)
       case "--dont-fork" :: tail =>
