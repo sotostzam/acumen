bad42b5f7379d31d17e1faa8abeb17c080fa863d
Jan Duracz <jan@duracz.net>
2013-05-29 04:46:30 +0200

    Revert "Merged changes in enclosure Regression test class."
    This reverts commit 08e6a46e830aa827cbb4e6b3b9209f8c91bb829b.

--- src/test/scala/acumen/interpreters/enclosure/Regression.scala
+++ src/test/scala/acumen/interpreters/enclosure/Regression.scala
@@ -13,21 +13,38 @@ object Regression extends Properties("Regression") {
 
   val FILE_SUFFIX_RESULT = ".result"
 
-  // FIXME: fail with error message when no files are loaded
   property("consistency") = {
     val models = readFiles("src/test/resources/acumen/interpreters/enclosure/models/", FILE_SUFFIX_MODEL)
     val results = readFiles("src/test/resources/acumen/interpreters/enclosure/expected/", FILE_SUFFIX_RESULT)
-
-    val resultKeySet = results.keySet
-    (models.size == results.size) && (!models.isEmpty) && models.forall {
-      case (name, model) =>
-        try {
-          val ast = Parser.run(Parser.prog, model)
-          val des = Desugarer.run(ast)
-          val enclosures = Interpreter.run(des).res
-          results.get(name) match {
-            case None =>
-              println(name + ".result is missing for comparison with result:\n" + enclosures)
+    if (models.size == 0) {
+      println("No models found!") 
+      false
+    } else {
+      val resultKeySet = results.keySet
+      models.forall {
+        case (name, model) =>
+          try {
+            val ast = Parser.run(Parser.prog, model)
+            val des = Desugarer.run(ast)
+            val enclosures = Interpreter.run(des).res
+            results.get(name) match {
+              case None =>
+                println(name + ".result is missing for comparison with result:\n" + enclosures)
+                false
+              case Some(result) =>
+                val success = enclosures.toString == result
+                if (!success) {
+                  println("expected: " + result)
+                  println("obtained: " + enclosures.toString)
+                  throw (new Error(name))
+                }
+                success
+            }
+          } catch {
+            case err =>
+              val name = err.getMessage
+              err.printStackTrace
+              println(name + ".acm INCONSISTENT")
               false
           }
       }
