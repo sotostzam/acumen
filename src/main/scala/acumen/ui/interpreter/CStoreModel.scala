sotostzam/acumen

commit e019e1a17956f760e688e672cdc9b24e49dff66d

Change how encloses are shown in the table to make parsing easier.
That is instead of having each column show a single interval, split the
interval into two columns and label the columns VAR.min and VAR.max.
This allows the exported results to more easily be imported into other
tools, such as Excel.

src/main/scala/acumen/ui/interpreter/EnclosureModel.scala
@@ -73,7 +73,7 @@ class EnclosureModel extends TraceModel with PlotModel with InterpreterModel {
       tableTimes(i) = timeGrouping(j)._1
 
       // prep the table array
-      tableData = Array.fill(plottables.size + 1){null:Array[String]}
+      tableData = Array.fill(plottables.size*2 + 1){null:Array[String]}
 
       // fill in the first column with the time value
       tableData(0) = tableTimes.map {t => "%f".format(t)}
@@ -85,27 +85,33 @@ class EnclosureModel extends TraceModel with PlotModel with InterpreterModel {
           val data = enclosures.slice(start,stop)
           val lo = data.map {encl => encl.loLeft}.min
           val hi = data.map {encl => encl.hiLeft}.max
-          "[%f,%f]".format(lo,hi)
+          ("%f".format(lo), "%f".format(hi))
         }
         def getRight(idx: Int) = {
           val (_,start,stop) = timeGrouping(idx)
           val data = enclosures.slice(start,stop)
           val lo = data.map {encl => encl.loRight}.min
           val hi = data.map {encl => encl.hiRight}.max
-          "[%f,%f]".format(lo,hi)
+          ("%f".format(lo), "%f".format(hi))
         }
-        val r = Array.fill(timeGrouping.size * 2 - 2){null:String}
+        val rLo = Array.fill(timeGrouping.size * 2 - 2){null:String}
+        val rHi = Array.fill(timeGrouping.size * 2 - 2){null:String}
         var i = 0; var j = 1
         while (j < timeGrouping.size) {
-          r(i) = getLeft(j)
-          i += 1
-          r(i) = getRight(j)
+          val (a,b) = getLeft(j)
+          rLo(i) = a
+          rHi(i) = b
+          i += 1;
+          val (c,d) = getRight(j)
+          rLo(i) = c
+          rHi(i) = d
           i += 1; j += 1
         }
-        tableData(idx) = r
+        tableData((idx-1)*2+1) = rLo
+        tableData((idx-1)*2+2) = rHi
       }
 
-      columnNames = IndexedSeq("time") ++ enclSeqs.map{case(n,_,_) => n}
+      columnNames = IndexedSeq("time") ++ enclSeqs.map{case(n,_,_) => List(n+".min", n+".max")}.flatten
     }
   }
 

commit a179fc70f846523e06541d99ccde4cedee8a8b83

Fix Plotter to show correct variable names when using the enclosure interpreter.
Basically another hack to patch what the hack in commit
e019e1a17956f760e688e672cdc9b24e49dff66d (Change how encloses are
shown in the table to make parsing easier.) broke.

Fixes ACUMEN-250.

src/main/scala/acumen/ui/interpreter/CStoreModel.scala
@@ -156,6 +156,7 @@ class CStoreModel extends TraceModel with InterpreterModel with PlotModel {
     } catch { case _ => None }
   }
 
+  override def getPlotTitle(col:Int) = d.columnNames(col)
   override def getColumnName(col:Int) = d.columnNames(col)
 
   override def isEmpty() = d.rowCount == 0

src/main/scala/acumen/ui/interpreter/EnclosureModel.scala
@@ -23,6 +23,7 @@ class EnclosureModel extends TraceModel with PlotModel with InterpreterModel {
     var tableTimes = Array.ofDim[Double](0)
     var tableData  = Array.ofDim[String](0,0)
     var columnNames : IndexedSeq[String] = IndexedSeq.empty[String]
+    var plotTitles : IndexedSeq[String] = IndexedSeq.empty[String]
 
     def recompute() = {
       times = es.map(_.domain.hiDouble).foldLeft(ArrayBuffer(es.head.domain.loDouble)) { case (res, t) => res += t }
@@ -111,6 +112,7 @@ class EnclosureModel extends TraceModel with PlotModel with InterpreterModel {
         tableData((idx-1)*2+2) = rHi
       }
 
+      plotTitles = IndexedSeq("time") ++ enclSeqs.map{case(n,_,_) => n}
       columnNames = IndexedSeq("time") ++ enclSeqs.map{case(n,_,_) => List(n+".min", n+".max")}.flatten
     }
   }
@@ -123,6 +125,8 @@ class EnclosureModel extends TraceModel with PlotModel with InterpreterModel {
   override def getValueAt(row: Int, column: Int) = data.tableData(column)(row)
 
   override def getDouble(row: Int, column: Int) = None
+
+  override def getPlotTitle(col: Int) = data.plotTitles(col)
 
   override def getColumnName(col: Int) = data.columnNames(col)
 

src/main/scala/acumen/ui/interpreter/InterpreterModel.scala
@@ -38,7 +38,7 @@ trait PlotModel {
   // These mebers may be called on the EDT:
   def getRowCount() : Int
   def getValueAt(row:Int, column:Int) : String
-  def getColumnName(col:Int) : String
+  def getPlotTitle(col:Int) : String
   def getDouble(row:Int, column:Int): Option[Double]
   def isEmpty(): Boolean
   // These members will not get called when the model is updating:

src/main/scala/acumen/ui/plot/PlotPanel.scala
@@ -265,12 +265,12 @@ class PlotPanel(pub:Publisher) extends Panel
           // corresponding column in the trace model
           val column = pd.columnIndices(hb)
           // name of the column
-          val name = model.getColumnName(column)
+          val name = model.getPlotTitle(column)
           // value of the column as a string
           var value = model.getValueAt(row, column).asInstanceOf[String]
           // FIXME: This is an incredible ugly hack to check if we area
           //   displaying enclosures instead of a real trace view.
-          if (model.getColumnName(0) == "time" && value(0) == '[') {
+          if (model.getPlotTitle(0) == "time" && value(0) == '[') {
             if (row == 0)
               value = "(-,%s)".format(value)
             else if (row == model.getRowCount() - 1)