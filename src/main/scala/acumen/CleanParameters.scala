sotostzam/acumen

commit e05a09107a5abc857845dd81c30393b17bbb0dd0

Move code to register valid simulator parameters into Common.

src/main/scala/acumen/interpreters/Common.scala
@@ -6,6 +6,10 @@ import util.Conversions._
 import scala.math._
 import Errors._
 
+//
+// Common stuff to CStore Interpreters
+//
+
 object Common {
  
   /* get the definition associated with a classname in p */
@@ -186,5 +190,12 @@ object Common {
          throw UnknownOperator(op)    
     }
   }
+
+  // register valid simulator parameters
+  List("time", "timeStep", "endTime", "nextStepType", "lastCreatedId").foreach { parm => 
+    acumen.CleanParameters.parms.registerParm(parm, acumen.CStoreInterpreterType)
+  }
+
+  val specialFields = List("nextChild","parent","className","seed1","seed2")
  
 }

src/main/scala/acumen/interpreters/newreference/Interpreter.scala
@@ -396,11 +396,4 @@ object Interpreter extends acumen.CStoreInterpreter {
         }
       }
     )
-
-
-  // a bit evil I know -- kevina
-  List("time", "timeStep", "endTime", "nextStepType", "lastCreatedId").foreach { parm => 
-    acumen.CleanParameters.parms.registerParm(parm, acumen.CStoreInterpreterType)
-  }
-
 }

src/main/scala/acumen/interpreters/reference/Interpreter.scala
@@ -385,10 +385,4 @@ object Interpreter extends acumen.CStoreInterpreter {
       }
     )
 
-
-  // a bit evil I know -- kevina
-  List("time", "timeStep", "endTime", "nextStepType", "lastCreatedId").foreach { parm => 
-    acumen.CleanParameters.parms.registerParm(parm, acumen.CStoreInterpreterType)
-  }
-
 }

commit 0d7e28489fd796f7068401e8677345c3a652cc11

Register parameters to be cleaned in the CleanParamater object.
If they are registered in some other object the registration code
might get called too late.

This fixes a bug introduced in commit e05a09107a5abc857845dd81c
("Move code to register valid simulator parameters into Common.")

src/main/scala/acumen/CleanParameters.scala
@@ -13,10 +13,22 @@ trait Parameters extends scala.collection.Map[String, List[InterpreterType]]
 }
 
 object CleanParameters {
+
   val parms = new scala.collection.mutable.ListMap[String, List[InterpreterType]] with Parameters {
     def registerParm(parm: String, intr: InterpreterType) = 
       update(parm, intr :: get(parm).getOrElse(Nil))
   }
+
+  // add paramaters for CStore based interpreters
+  acumen.interpreters.Common.simulatorFields.foreach { parm => 
+    parms.registerParm(parm, acumen.CStoreInterpreterType)
+  }
+  // add paramters for Enclosure based interpreters
+  acumen.interpreters.enclosure.Parameters.defaults.foreach {
+    case (parm, _) =>
+      acumen.CleanParameters.parms.registerParm(parm, acumen.EnclosureInterpreterType)
+  }
+
 
   case object AssignToSimulator extends Throwable
 

src/main/scala/acumen/interpreters/Common.scala
@@ -192,9 +192,7 @@ object Common {
   }
 
   // register valid simulator parameters
-  List("time", "timeStep", "endTime", "resultType", "lastCreatedId").foreach { parm => 
-    acumen.CleanParameters.parms.registerParm(parm, acumen.CStoreInterpreterType)
-  }
+  val simulatorFields = List("time", "timeStep", "endTime", "resultType", "lastCreatedId")
 
   val specialFields = List("nextChild","parent","className","seed1","seed2")
  

src/main/scala/acumen/interpreters/enclosure/Parameters.scala
@@ -55,8 +55,4 @@ object Parameters {
     "minComputationImprovement" -> 0.0001,
     "splittingDegree" -> 1,
     "maxIterations" -> 100)
-  defaults.foreach {
-    case (parm, _) =>
-      acumen.CleanParameters.parms.registerParm(parm, acumen.EnclosureInterpreterType)
-  }
 }