sotostzam/acumen

commit 2f738e228205ed326f4938161de9bea6a0125a6e

Merge GraphicalMain and Main command line handling.
Also provide "--help" for normal help and "--full-help" to provide
help with the experimental parts.

The "--example" command line option is replaces with the more sane
"--model" option.  The syntax of the command-line main commands
changed so the command is first and the file is second for example
instead of "model.acm last" use "last model.acm".  This provides a
more uniform syntax inline with other multi-use binaries.

Towards ACUMEN-80.

src/main/scala/acumen/ui/GraphicalMain.scala
@@ -23,84 +23,29 @@ import javax.swing.event.DocumentEvent
 import swing._
 import swing.event._
 
-object ThreeDState extends Enumeration {
-  val ERROR,DISABLE,LAZY,ENABLE = Value
-}
-
 object GraphicalMain extends SimpleSwingApplication {
   // Swing debugging
   javax.swing.RepaintManager.setCurrentManager(new debug.CheckThreadViolationRepaintManager)
   debug.EventDispatchThreadHangMonitor.initMonitoring()
 
   val osname = System.getProperty("os.name").toLowerCase
-  var need_quartz = false
   // ThreeDStates default to lazy as loading it can cause performance
   // problems on some platforms (mainly Linux).  On Windows load it
   // eagerly as the 3D code was tested the most on Windows.  On OS X
   // load it eagerly as loading as loading lazily can cause a crash
   // if the libraries are not available.
-  var threeDState = ThreeDState.LAZY
-  if (osname.startsWith("windows ")) {
-    threeDState = ThreeDState.ENABLE
-  } else if (osname.contains("os x")) {
-    if (!(System.getProperty("apple.awt.graphics.UseQuartz") == "true")) {
-      threeDState = ThreeDState.DISABLE
-      need_quartz = true
+  def fixupThreeDState = if (Main.threeDState == null) {
+    if (osname.startsWith("windows ")) {
+      Main.threeDState = ThreeDState.ENABLE
+    } else if (osname.contains("os x")) {
+      if (!(System.getProperty("apple.awt.graphics.UseQuartz") == "true")) {
+        Main.threeDState = ThreeDState.DISABLE
+        Main.need_quartz = true
+      } else {
+        Main.threeDState = ThreeDState.ENABLE
+      }
     } else {
-      threeDState = ThreeDState.ENABLE
-    }
-  }
-  var disableNewPlot = true
-  var openFile: File = null
-  var autoPlay = false
-  var interpreterString = ""
-  var useCompletion = true
-  var useTemplates = false
-  var dontFork = false
-  var synchEditorWithBrowser = true // Synchronize code editor with file browser
-  var enclosureInterpreter = interpreters.enclosure.Interpreter
-  var extractHA = false
-
-  def parseOpts(args: List[String]) {
-    args match {
-      case Nil =>
-      case ("--semantics"|"--interpreter"|"-i") :: i :: tail =>
-        interpreterString = i
-      case ("--enable-3d" | "--3d") :: tail => 
-        threeDState = ThreeDState.ENABLE; parseOpts(tail)
-      case ("--lazy-3d") :: tail => 
-        threeDState = ThreeDState.LAZY; parseOpts(tail)
-      case ("--disable-3d" | "--no-3d") :: tail => 
-        need_quartz = false
-        threeDState = ThreeDState.DISABLE; parseOpts(tail)
-      case ("--enable-newplot" | "--newplot") :: tail => 
-        disableNewPlot = false; parseOpts(tail)
-      case ("--disable-newplot" | "--no-newplot") :: tail => 
-        disableNewPlot = true; parseOpts(tail)
-      case "--example" :: name :: tail =>
-        openFile = new File("examples", name + ".acm"); parseOpts(tail)
-      case "--play" :: tail =>
-        autoPlay = true; parseOpts(tail)
-      case "--enable-completion" :: tail =>
-        useCompletion = true; parseOpts(tail)
-      case "--disable-completion" :: tail =>
-        useCompletion = false; parseOpts(tail)
-      case "--templates" :: tail =>
-        useTemplates = true; parseOpts(tail)
-      case "--dont-fork" :: tail =>
-        dontFork = true; parseOpts(tail)
-      case "--extract-ha" :: tail =>
-        extractHA = true; parseOpts(tail)
-      case opt ::  tail if opt.startsWith("-") =>
-        System.err.println("Unrecognized Option: " + opt)
-        exit(1)
-      case fn :: tail => 
-        openFile = new File(fn)
-        if (!openFile.exists) {
-          System.err.println("File not found: " + openFile)
-          exit(1)
-        }
-        parseOpts(tail)
+      Main.threeDState = ThreeDState.LAZY
     }
   }
 
@@ -109,8 +54,8 @@ object GraphicalMain extends SimpleSwingApplication {
 
   def maybeFork(args: Array[String]) {
     val maxMem = Runtime.getRuntime().maxMemory()
-    val shouldFork = maxMem < MIN_MAX_MEM || need_quartz
-    val mayFork = !dontFork
+    val shouldFork = maxMem < MIN_MAX_MEM || Main.need_quartz
+    val mayFork = !Main.dontFork
     if (shouldFork && mayFork) {
       val separator = System.getProperty("file.separator");
       val classpath = System.getProperty("java.class.path");
@@ -120,8 +65,8 @@ object GraphicalMain extends SimpleSwingApplication {
       realArgs.add(path)
       realArgs.addAll(bean.getInputArguments())
       realArgs.add("-Xmx1g")
-      val quartz = if (need_quartz) "-Dapple.awt.graphics.UseQuartz=true" else ""
-      if (need_quartz) realArgs.add(quartz)
+      val quartz = if (Main.need_quartz) "-Dapple.awt.graphics.UseQuartz=true" else ""
+      if (Main.need_quartz) realArgs.add(quartz)
       realArgs.add("-cp")
       realArgs.add(classpath)
       realArgs.add("acumen.ui.GraphicalMain")
@@ -171,16 +116,14 @@ object GraphicalMain extends SimpleSwingApplication {
   }
 
   override def main(args: Array[String]) {
-    if (args.size > 0 && (args(0) == "--semantics" || !args(0).startsWith("-"))) {
-      acumen.Main.main(args)
-    } else {
-      parseOpts(args.toList)
-      maybeFork(args)
-      super.main(args)
-    }
+    if (Main.positionalArgs.size() > 1)
+      Main.openFile = Main.checkFile(Main.positionalArgs(1))
+    maybeFork(args)
+    super.main(args)
   }
 
   def top = {
+    fixupThreeDState
     try {
       App.init
     } catch {

build.sbt
@@ -4,7 +4,7 @@ version := "10-devel"
 
 scalaVersion := "2.9.3"
 
-theMainClass := "acumen.ui.GraphicalMain"
+theMainClass := "acumen.Main"
 
 libraryDependencies ++= Seq(
   "org.scala-lang" % "scala-swing" % "2.9.3",

src/main/scala/acumen/Errors.scala
@@ -122,11 +122,6 @@ object Errors {
 
   /* Command-line errors */
 
-  case class BadProgramOptions(valid:Seq[String]) extends AcumenError {
-    override def getMessage =
-      "Bad command-line options. Valid options are " + valid.mkString(", ") + "."
-  }
-
   case class UnrecognizedInterpreterString(theString: String) extends AcumenError {
     override def getMessage = 
       "Unrecognized interpreter string: " + theString

src/main/scala/acumen/Main.scala
@@ -11,25 +11,141 @@ import Pretty._
 import com.sun.j3d.utils.applet.MainFrame
 
 import java.net.{Socket, InetAddress, ServerSocket}
-import acumen.Errors.BadProgramOptions
+
+import scala.collection.mutable.ArrayBuffer
+
+object ThreeDState extends Enumeration {
+  val ERROR,DISABLE,LAZY,ENABLE = Value
+}
 
 object Main {
 
-  var portNo : Int = 9999
-  var serverSocket : ServerSocket = null
-  var serverMode : Boolean = false
-  var serverBufferedReader : BufferedReader = null
-  var serverBufferedWriter : BufferedWriter = null
+  //
+  // What should be in Main
+  //
 
-  def send_recv(s: String) : String = {
-    serverBufferedWriter.write(s)
-    serverBufferedWriter.newLine()
-    serverBufferedWriter.flush()
-    serverBufferedReader.readLine()
+  var need_quartz = false
+  var threeDState: ThreeDState.Value = null
+  var disableNewPlot = true
+  var autoPlay = false
+  var openFile: File = null
+  var interpreter : Interpreter = null
+  var useCompletion = true
+  var useTemplates = false
+  var dontFork = false
+  var synchEditorWithBrowser = true // Synchronize code editor with file browser
+  var extractHA = false
+  var displayHelp = "none"
+
+  var positionalArgs = new ArrayBuffer[String]
+
+  // Note: lots of options and commands have alias but in general only
+  //  the preferred version should be showed.  Lots also have
+  //  enable|disable|no version but only one that changes the default
+  //  behavior should be displayed
+  def optsHelp = Array(
+    "--help                  this help message",
+    "-i|--semantics "+ Main.interpreterHelpString,
+    "                        select interpreter to use",
+    "--model <file>          model file to open",
+    "--3d|--lazy-3d|--no-3d  controls the default state for the 3d tab",
+    "--newplot               enable experimental plotter",
+    "--play                  automatically run the model",
+    "--disable-completion    disable code completion in the source code editor",
+    "--dont-fork             disable auto-forking of a new JVM when required")
+  def experimentalOptsHelp = Array(
+    "--full-help",
+    "--extract-ha",
+    "--templates             enables template expansion in the source code editor."
+  )
+  def commandHelp = Array(
+    "ui [<file>]             starts the U.I."
+  )
+  def experimentalCommandHelp = Array(
+    "pretty <file>           pretty print model",
+    "desugar <file>          pretty print desuguared model",
+    "last <file>             run model and print final result",
+    "trace <file>            run model and print trace output",
+    "time <file>             time time it takes to run model",
+    "",
+    "extract <file>          extract H.A. and print result",
+    "compile <file>          compile model to C++",
+    "typecheck <file>        run type checker",
+    "",
+    "bench <file> <start> <stop> [<warmup> [<repeat>]]",
+    "                        parallel benchmark",
+    "bench-gnuplot ...       like \"bench\" but output gnuplot script",
+    "bench-enclosures <file> <prefix> <repeat> [parms]",
+    "                        enclosure benchmark", //  see --help-bench-enclosures",
+    "listen <file> <portnum>",
+    "",
+    "examples                record reference outputs for test suite"
+    //"2d|3d|java2d|java3d" // have no clue what these do! -- kevina
+    //"fromJson",
+  )
+
+  def usage() = {
+    System.err.println("Try --help for more information.")
+    System.exit(1)
   }
 
-  def as_ctrace(trace: InterpreterRes) = {
-    trace match {case CStoreRes(r) => r; case _ => null}    
+  def parseArgs(args: List[String]) : Unit = try {
+    args match {
+      case Nil =>
+      case ("--help" ) :: tail =>
+        displayHelp = "normal"; parseArgs(tail)
+      case ("--help-full"|"--full-help") :: tail =>
+        displayHelp = "full"; parseArgs(tail)
+      case ("--help-bench-enclosures") :: tail =>
+        displayHelp = "bench-enclosures"; parseArgs(tail)
+      case ("--semantics"|"--interpreter"|"-i") :: i :: tail =>
+        interpreter = selectInterpreter(i); parseArgs(tail)
+      case ("--model") :: f :: tail =>
+        openFile = checkFile(f); parseArgs(tail)
+      case ("--enable-3d" | "--3d") :: tail => 
+        threeDState = ThreeDState.ENABLE; parseArgs(tail)
+      case ("--lazy-3d") :: tail => 
+        threeDState = ThreeDState.LAZY; parseArgs(tail)
+      case ("--disable-3d" | "--no-3d") :: tail => 
+        need_quartz = false
+        threeDState = ThreeDState.DISABLE; 
+        parseArgs(tail)
+      case ("--enable-newplot" | "--newplot") :: tail => 
+        disableNewPlot = false; parseArgs(tail)
+      case ("--disable-newplot" | "--no-newplot") :: tail => 
+        disableNewPlot = true; parseArgs(tail)
+      case "--play" :: tail =>
+        autoPlay = true; parseArgs(tail)
+      case "--enable-completion" :: tail =>
+        useCompletion = true; parseArgs(tail)
+      case "--disable-completion" :: tail =>
+        useCompletion = false; parseArgs(tail)
+      case "--templates" :: tail =>
+        useTemplates = true; parseArgs(tail)
+      case "--dont-fork" :: tail =>
+        dontFork = true; parseArgs(tail)
+      case "--extract-ha" :: tail =>
+        extractHA = true; parseArgs(tail)
+      case opt ::  tail if opt.startsWith("-") =>
+        System.err.println("Unrecognized Option: " + opt)
+        usage()
+      case arg :: tail => 
+        positionalArgs += arg
+        parseArgs(tail)
+    }
+  } catch {
+    case e : AcumenError => 
+      System.err.println(e.getMessage())
+      usage()
+  }
+
+  def checkFile(fn: String) = {
+    val openFile = new File(fn)
+    if (!openFile.exists) {
+      System.err.println("File not found: " + openFile)
+      exit(1)
+    }
+    openFile
   }
 
   def selectInterpreter(args0: String*) : Interpreter = {
@@ -81,26 +197,71 @@ object Main {
     }
   }
 
-  def main(args: Array[String]): Unit = {
-    val I = interpreters.reference.Interpreter
-    //val I = new interpreters.imperative.parallel.Interpreter(2)
-
-    try {
-      /* See if user wants to choose a specific interpreter. */
-      val (i: Interpreter, firstNonSemanticsArg: Int) = args(0) match {
-        case "--semantics" => (selectInterpreter(args(1)), 2)
-        case _ => (interpreters.reference.Interpreter, 0)
+  def main(args: Array[String]) : Unit = {
+    parseArgs(args.toList)
+    if (displayHelp == "normal") {
+      println("Options: ");
+      optsHelp.foreach{line => println("  " + line)}
+    } else if (displayHelp == "full") {
+      println("Usage: acumen [options] [command]")
+      println("");
+      println("Options: ");
+      optsHelp.foreach{line => println("  " + line)}
+      println("");
+      println("Experimental options:")
+      experimentalOptsHelp.foreach{line => println("  " + line)}
+      println("");
+      println("Commands: ");
+      commandHelp.foreach{line => println("  " + line)} 
+      println("");
+      println("Experimental commands:");
+      experimentalCommandHelp.foreach{line => println("  " + line)}
+    } else {
+      if (positionalArgs.size == 0) "ui" else positionalArgs(0) match {
+        case "ui" => 
+          ui.GraphicalMain.main(args)
+        case "examples"|"record-reference-outputs" => 
+          examples
+        case _ =>
+          origMain(positionalArgs.toArray)
       }
+    }
+  }
+
+  //
+  // Other stuff that should eventually be factored out
+  //
+
+  var portNo : Int = 9999
+  var serverSocket : ServerSocket = null
+  var serverMode : Boolean = false
+  var serverBufferedReader : BufferedReader = null
+  var serverBufferedWriter : BufferedWriter = null
+
+  def send_recv(s: String) : String = {
+    serverBufferedWriter.write(s)
+    serverBufferedWriter.newLine()
+    serverBufferedWriter.flush()
+    serverBufferedReader.readLine()
+  }
+
+  def as_ctrace(trace: InterpreterRes) = {
+    trace match {case CStoreRes(r) => r; case _ => null}    
+  }
+
+  def origMain(args: Array[String]) : Unit = {
+    try {
+      val i = interpreter
 
       /* Read the Acumen source, parse, pre-process and interpret it. */
-      lazy val in = new InputStreamReader(new FileInputStream(args(firstNonSemanticsArg)))
+      lazy val in = new InputStreamReader(new FileInputStream(args(1)))
       lazy val ast = Parser.run(Parser.prog, in)
       lazy val desugared = Desugarer.run(ast)
       lazy val final_out = desugared // final output after all passes
       lazy val trace = i.run(final_out)
       lazy val ctrace = as_ctrace(trace)
       /* Perform user-selected action. */
-      args(firstNonSemanticsArg + 1) match {
+      args(0) match {
         case "compile" => 
           val typeChecker = new TypeCheck(desugared)
           val res = typeChecker.run()
@@ -125,9 +286,9 @@ object Main {
           val x = JSon.fromJSON(JSon.toJSON(st).toString)
           println(x)
         case "listen" =>
-          println("Model: " + args(firstNonSemanticsArg))
+          println("Model: " + args(0))
           serverMode = true
-          portNo = args(firstNonSemanticsArg + 2).toInt
+          portNo = args(2).toInt
           serverSocket = new ServerSocket(portNo)
           println("Listening on port " + portNo)
           val socket = serverSocket.accept()
@@ -143,7 +304,7 @@ object Main {
           val endTime = System.currentTimeMillis()
           println("Time to run: " + (endTime - startTime)/1000.0)
         case "bench" =>
-          val offset = firstNonSemanticsArg + 1 + 1;
+          val offset = 2;
           val start: Int = Integer.parseInt(args(offset + 0))
           val stop: Int = Integer.parseInt(args(offset + 1))
           val warmup : Int = if (args.size > offset + 2) Integer.parseInt(args(offset+2)) else 0
@@ -161,7 +322,7 @@ object Main {
           }
         // the first six lines are shared with the "bench" case and would ideally not be repeated
         case "bench-gnuplot" =>
-          val offset = firstNonSemanticsArg + 1 + 1;
+          val offset = 2;
           val start: Int = Integer.parseInt(args(offset + 0))
           val stop: Int = Integer.parseInt(args(offset + 1))
           val warmup : Int = if (args.size > offset + 2) Integer.parseInt(args(offset+2)) else 0
@@ -183,42 +344,42 @@ object Main {
           // outputting progress information as is done in the "bench" case
           println(Gnuplot.script(data))
         case "bench-enclosures" => 
-          BenchEnclosures.run(i, final_out, args, firstNonSemanticsArg + 2)
+          BenchEnclosures.run(i, final_out, args, 2)
         case "trace" =>
           trace.print
-        case "examples"|"record-reference-outputs" =>
-          var somethingUpdated = false
-          Examples.cstoreExamplesAction{(dn, f) =>
-            val loc = Examples.expectLoc
-            val resFile = Examples.resultFile(loc, dn, f)
-            if (resFile.exists) {
-              println("skipping " + f + "")
-            } else {
-              somethingUpdated = true
-              try {
-                Examples.writeExampleResult(loc, dn, f, interpreters.reference.Interpreter)
-              } catch {
-                case e => 
-                  println("ERROR when processing " + f + ":")
-                  println("  " + e)
-              }
-              println("PROCESSED " + f)
-            }
-          }
-          if (somethingUpdated) {
-            println("Results updated.  Be sure to git add & commit the updated files as appropriate.")
-          }
         case what =>
-          println(what)
-          throw BadProgramOptions(
-            List("pretty", "desugar", "3d", "2d", "java2d",
-              "java3d", "json", "last", "bench from to", "bench-enclosures ...", "trace"))
+          System.err.println("Unrecognized Command: " + what)
+          usage()
       }
     } catch {
       case e: AcumenError =>
         System.err.println(e.getMessage)
         System.exit(1)
       case e => throw e
+    }
+  }
+
+  def examples = {
+    var somethingUpdated = false
+    Examples.cstoreExamplesAction{(dn, f) =>
+      val loc = Examples.expectLoc
+      val resFile = Examples.resultFile(loc, dn, f)
+      if (resFile.exists) {
+        println("skipping " + f + "")
+      } else {
+        somethingUpdated = true
+        try {
+          Examples.writeExampleResult(loc, dn, f, interpreters.reference.Interpreter)
+        } catch {
+          case e => 
+            println("ERROR when processing " + f + ":")
+            println("  " + e)
+        }
+        println("PROCESSED " + f)
+      }
+    }
+    if (somethingUpdated) {
+      println("Results updated.  Be sure to git add & commit the updated files as appropriate.")
     }
   }
 }

src/main/scala/acumen/ui/App.scala
@@ -198,15 +198,15 @@ class App extends SimpleSwingApplication {
     val synchButton = new JCheckBox()
     synchButton.setAction(new AbstractAction("Synch File Browser and Editor") {
       override def actionPerformed(e: java.awt.event.ActionEvent) {
-        GraphicalMain.synchEditorWithBrowser = !GraphicalMain.synchEditorWithBrowser
-        if (GraphicalMain.synchEditorWithBrowser)
+        Main.synchEditorWithBrowser = !Main.synchEditorWithBrowser
+        if (Main.synchEditorWithBrowser)
           codeArea.currentFile match {
             case Some(file) => fileBrowser.fileTree.focus(file)
             case None => fileBrowser.fileTree.refresh
           }
       }
     })
-    synchButton.setSelected(GraphicalMain.synchEditorWithBrowser)
+    synchButton.setSelected(Main.synchEditorWithBrowser)
     val toolbar = new JToolBar()
     toolbar.setFloatable(false)
     synchButton.setFocusable(false)
@@ -259,7 +259,7 @@ class App extends SimpleSwingApplication {
   }
   var newPlotView: plot.JFreePlotTab = null
   var newPlotTab: BorderPanel = null
-  if (!GraphicalMain.disableNewPlot) {
+  if (!Main.disableNewPlot) {
     newPlotView = new plot.JFreePlotTab
     newPlotTab = new BorderPanel {
       //TODO Implement and add something like pointedView for the new plotting code
@@ -269,16 +269,16 @@ class App extends SimpleSwingApplication {
   }
 
   val traceTab = new ScrollPane(traceTable)
-  var threeDtab = if (GraphicalMain.threeDState == ThreeDState.DISABLE) {
+  var threeDtab = if (Main.threeDState == ThreeDState.DISABLE) {
     console.log("Acumen3D disabled.")
     console.newLine
-    if (GraphicalMain.need_quartz) {
+    if (Main.need_quartz) {
       new threeD.DisabledThreeDTab("3D visualization disabled due to performace problems on Mac OS X. \n\nTo enable restart Java with -Dapple.awt.graphics.UseQuartz=true or use --3d to force 3D to be enabled.")
     } else {
       new threeD.DisabledThreeDTab("3D visualization disabled on the command line.")
     }
     //null
-  } else if (GraphicalMain.threeDState == ThreeDState.LAZY) {
+  } else if (Main.threeDState == ThreeDState.LAZY) {
     new threeD.DisabledThreeDTab("3D visualization will be enabled when needed.")
   } else {
     start3D
@@ -286,7 +286,7 @@ class App extends SimpleSwingApplication {
 
   def start3D = try {
     val res = new threeD.ThreeDTab(controller)
-    GraphicalMain.threeDState = ThreeDState.ENABLE
+    Main.threeDState = ThreeDState.ENABLE
     res
   } catch {
     case e =>
@@ -294,7 +294,7 @@ class App extends SimpleSwingApplication {
       console.newLine
       console.log("Disabling 3D Tab.")
       console.newLine
-      GraphicalMain.threeDState = ThreeDState.ERROR
+      Main.threeDState = ThreeDState.ERROR
       val errors = new StringWriter()
       e.printStackTrace(new PrintWriter(errors))
       new threeD.DisabledThreeDTab("Acumen 3D disabled.\nError loading Java3D: " + e +
@@ -471,20 +471,20 @@ class App extends SimpleSwingApplication {
       val pwl = new RadioMenuItem("") {
         action = pwlHybridSolverAction
         enableWhenStopped(this)
-        selected = false // GraphicalMain.useEnclosures && 
+        selected = false // Main.useEnclosures && 
           //enclosure.Interpreter.strategy.eventEncloser.getClass == classOf[PWLEventEncloser] 
       }
       val et = new RadioMenuItem("") {
         action = eventTreeHybridSolverAction
         enableWhenStopped(this) 
-        selected = false // GraphicalMain.useEnclosures &&
+        selected = false // Main.useEnclosures &&
           //enclosure.Interpreter.strategy.eventEncloser.getClass == classOf[TreeEventEncloser]
       }
       val bg = new ButtonGroup(ref, newRef, impr, par, pwl, et)
       val ls = new CheckMenuItem("") {
         action = contractionAction
         enabledWhenStopped += (this, () => interpreter.interpreter.getClass == enclosure.Interpreter.getClass)
-        enabled = false //GraphicalMain.useEnclosures
+        enabled = false //Main.useEnclosures
         selected = enclosure.Interpreter.strategy.eventEncloser.ivpSolver.getClass == classOf[LohnerSolver]
         /* Enable/disable Contraction menu item depending on the chosen semantics */
         for (b <- bg.buttons) listenTo(b) 
@@ -569,7 +569,7 @@ class App extends SimpleSwingApplication {
     val intr = Main.selectInterpreter(args:_*)
     interpreter = InterpreterCntrl.cntrlForInterpreter(intr);
   }
-  setInterpreter(GraphicalMain.interpreterString)
+  interpreter = InterpreterCntrl.cntrlForInterpreter(Main.interpreter);
   interpreter.interpreter.id.toList match {
     case "reference" :: _=> bar.semantics.ref.selected = true
     case "newreference" :: _ => bar.semantics.newRef.selected = true
@@ -622,7 +622,7 @@ class App extends SimpleSwingApplication {
   // enable 3d if required
   reactions += {
     case Stopped =>
-      if (GraphicalMain.threeDState == ThreeDState.LAZY &&
+      if (Main.threeDState == ThreeDState.LAZY &&
         !controller.threeDData._3DData.isEmpty)
         views.shouldEnable3D = true
       views.possibleEnable3D
@@ -729,7 +729,7 @@ class App extends SimpleSwingApplication {
   actor.publish(Stopped)
   actor.publish(ViewChanged(views.selection.index))
 
-  if (GraphicalMain.autoPlay)
+  if (Main.autoPlay)
     upperButtons.bPlay.doClick
 }
 

src/main/scala/acumen/ui/interpreter/EnclosureCntrl.scala
@@ -77,7 +77,7 @@ class EnclosureCntrl(val interpreter: RecursiveInterpreter) extends InterpreterC
       val ast = Parser.run(Parser.prog, progText)
       //val dif = SD.run(ast)
       val des = Desugarer.run(ast)
-      val des2 = if (GraphicalMain.extractHA)
+      val des2 = if (Main.extractHA)
                    new Extract(des).res
                  else
                    des

src/main/scala/acumen/ui/interpreter/InterpreterCntrl.scala
@@ -28,7 +28,7 @@ abstract class InterpreterActor(val progText : String, val consumer : Actor) ext
     val ast = Parser.run(Parser.prog, progText)
     val dif = SD.run(ast)
     val des = Desugarer.run(dif)
-    val des2 = if (GraphicalMain.extractHA)
+    val des2 = if (Main.extractHA)
                  new Extract(des).res
                else
                  des

src/main/scala/acumen/ui/tl/CodeArea.scala
@@ -86,13 +86,13 @@ class CodeArea extends Panel with TreeSelectionListener {
     sta.setHighlightCurrentLine(false)
     sta.setTabSize(2)
     sta.setTabsEmulated(true) // Use soft tabs
-    if (GraphicalMain.useCompletion) {
+    if (Main.useCompletion) {
       val completionProvider = createCompletionProvider(sta)
       val autoCompletion = new AutoCompletion(completionProvider)
       autoCompletion setTriggerKey KeyStroke.getKeyStroke("TAB")
       autoCompletion install sta
     }
-    if (GraphicalMain.useTemplates) {
+    if (Main.useTemplates) {
       RSyntaxTextArea setTemplatesEnabled true
       createCodeTemplateManager
     }
@@ -289,7 +289,7 @@ class CodeArea extends Panel with TreeSelectionListener {
 
   /* Listen for selection events in FileTree browser. */
   def valueChanged(e: TreeSelectionEvent) {
-    if (GraphicalMain.synchEditorWithBrowser) {
+    if (Main.synchEditorWithBrowser) {
       val lpc = e.getPath.getLastPathComponent
       val file =
         if (lpc.isInstanceOf[TreeFile]) lpc.asInstanceOf[TreeFile]
@@ -315,17 +315,9 @@ class CodeArea extends Panel with TreeSelectionListener {
     textArea setForeground Color.gray
   }
 
-  if (GraphicalMain.openFile != null) {
-    try {
-      loadFile(GraphicalMain.openFile)
-      notifyPathChangeListeners
-    }
-    catch {
-      case e =>
-        System.err.println("Unable To Open File: ")
-        System.err.println("  " + e)
-        exit(2)
-    }
+  if (Main.openFile != null) {
+    loadFile(Main.openFile)
+    notifyPathChangeListeners
   }
 
   /* Font management */

commit d46d39847cb0a1a78d760479a101e74ea734ab8d

Fix a bug that caused the forking code to brake when any command line parameters are specified.
This broke in commit 2f738e22, "Merge GraphicalMain and Main command
line handling.".

src/main/scala/acumen/ui/GraphicalMain.scala
@@ -69,7 +69,7 @@ object GraphicalMain extends SimpleSwingApplication {
       if (Main.need_quartz) realArgs.add(quartz)
       realArgs.add("-cp")
       realArgs.add(classpath)
-      realArgs.add("acumen.ui.GraphicalMain")
+      realArgs.add("acumen.Main")
       realArgs.addAll(args.toList)
       realArgs.add("--dont-fork")
       val processBuilder = new ProcessBuilder(realArgs)