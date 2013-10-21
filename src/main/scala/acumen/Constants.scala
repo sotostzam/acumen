a580a40d1db9429bb1297e22aebd42b38f74a563
Kevin Atkinson <k@kevina.org>
2013-10-21 23:06:22 +0200

    Make "pi" a constant rather than having it as a keyword.

--- src/main/scala/acumen/Constants.scala
+++ src/main/scala/acumen/Constants.scala
@@ -2,12 +2,17 @@ package acumen
 
 object Constants
 {
+  // this is a separate variable so it can be reliably tested for
+  // equality
   val PI = GDouble(math.Pi)
 
   def color(red: Double, green: Double, blue: Double) 
     = ExprVector(List(Lit(GDouble(red)), Lit(GDouble(green)), Lit(GDouble(blue))))
 
   val predefined : Map[String,Expr] = Map(
+    // mathematical constants
+    "pi" -> Lit(PI),
+    // colors
     "red" -> color(1,0,0),
     "green" -> color(0,1,0),
     "blue" -> color(0,0,1),
--- src/main/scala/acumen/Parser.scala
+++ src/main/scala/acumen/Parser.scala
@@ -87,7 +87,7 @@ object Parser extends MyStdTokenParsers {
     List("for", "end", "if", "else", "create", "move", "in",
       "terminate", "class", "sum", "true", "false",
       "private", "switch", "case", "Continuous", "Discrete", "none", "type", "claim",
-      "pi","let")
+      "let")
 
   /* token conversion */
 
@@ -298,7 +298,6 @@ object Parser extends MyStdTokenParsers {
   def atom: Parser[Expr] =
     ("sum" ~! expr ~! "for" ~! name ~! "in" ~! expr ~! "if" ~! expr ^^ { case _ ~ e ~ _ ~ i ~ _ ~ c ~ _ ~ t => Sum(e, i, c, t) }
       | interval
-      | constant
       | "type" ~! parens(className) ^^ { case _ ~ cn => TypeOf(cn) }
       | name >> { n => args(expr) ^^ { es => Op(n, es) } | success(Var(n)) }
       | brackets(repsep(expr, ",")) ^^ ExprVector
@@ -309,9 +308,6 @@ object Parser extends MyStdTokenParsers {
 //    nlit ~ ".." ~ nlit ^^ { case lo ~ ".." ~ hi => ExprInterval(lo,hi) }
       "[" ~> nlit ~ ".." ~ nlit <~ "]" ^^ { case lo ~ ".." ~ hi => ExprInterval(lo,hi) }
 
-  def constant =
-    "pi" ^^^ Lit(Constants.PI)
-      
   def lit = (gint | gfloat | gstr) ^^ Lit
 
   def name: Parser[Name] =
