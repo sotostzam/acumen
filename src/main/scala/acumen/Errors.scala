9c68385ef6f8c3a82e23b572a65869717b1520d2
Adam Duracz <adam@duracz.net>
2013-11-02 14:49:48 +0100

    Corrected duplicate discrete assingment check.
    Now all discrete assignments are registered (even identify
    assignments, i.e. those that do not change the Store), in order
    to always detect duplicate assignments to the same lhs.
    
    Also, duplicate assingments are now reported using a custom
    AcumenError, i.e. an object of a subclass to DuplicateAssingment.
    
    This change was implemented in both the standard and experimental
    reference interpreters.
    
    Test cases added based on model attached to ACUMEN-348.

--- src/main/scala/acumen/Errors.scala
+++ src/main/scala/acumen/Errors.scala
@@ -106,6 +106,16 @@ object Errors {
     override def getMessage = 
       "No case matching " + pprint(gv) + "."
   }
+  sealed abstract class DuplicateAssingment(x:Name) extends AcumenError {
+    def getMessage(kind: String) = 
+      "Repeated " + kind + " assignment to variable (" + x.x + "'" * x.primes + ") is not allowed."
+  }
+  case class DuplicateDiscreteAssingment(x:Name) extends DuplicateAssingment(x) {
+    override def getMessage = super.getMessage("discrete")
+  }
+  case class DuplicateContinuousAssingment(x:Name) extends DuplicateAssingment(x) {
+    override def getMessage = super.getMessage("continuous")
+  }
   case class BadLhs() extends AcumenError {
     override def getMessage = 
       "The left hand-side of an assignment must be of the form 'e.x'."
