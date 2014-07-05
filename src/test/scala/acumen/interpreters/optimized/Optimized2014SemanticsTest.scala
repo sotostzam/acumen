3d1819aae40bb4a8dfafe3ef0118aec688393a37
Kevin Atkinson <k.emg@kevina.org>
2014-07-05 03:08:54 +0200

    Fix bug in hodgkin-huxley model that was causing problems for the Optimized2014 interpreter.

--- src/test/scala/acumen/interpreters/optimized/Optimized2014SemanticsTest.scala
+++ src/test/scala/acumen/interpreters/optimized/Optimized2014SemanticsTest.scala
@@ -8,12 +8,5 @@ class Optimized2014SemanticsTest extends Traditional2014Tests {
 
   override def semantics = SemanticsImpl.Opt2014
 
-  override def examplesSkip(path: String) = {
-    super.examplesSkip(path) || 
-    // This one seems okay, but the number of iterations is
-    // different
-    path.endsWith("/hodgkin-huxley.acm")
-  }
-
 }
 
