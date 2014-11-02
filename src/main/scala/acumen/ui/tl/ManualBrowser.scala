06f399d85656ce7b5b34d646320d9e1b0dde5687
Adam Duracz <adam@duracz.net>
2014-11-02 18:26:30 +0100

    Make links clickable in ManualBrowser.
    Clicking a link will try to open it in an external browser.
    This will only work with Java 6 or later. An error message is output otherwise,
    asking the user to upgrade or use the online version of the manual.

--- src/main/scala/acumen/ui/tl/ManualBrowser.scala
+++ src/main/scala/acumen/ui/tl/ManualBrowser.scala
@@ -1,13 +1,18 @@
 package acumen.ui.tl
 
-import scala.swing.Frame
+import java.awt.Desktop
+import java.net.URL
 import javax.swing.JEditorPane
 import javax.swing.JScrollPane
 import javax.swing.WindowConstants
 import javax.swing.border.EmptyBorder
+import javax.swing.event.HyperlinkListener
+import javax.swing.event.HyperlinkEvent
 import javax.swing.text.Document
 import javax.swing.text.html.HTMLDocument
-import java.net.URL
+import scala.swing.Frame
+import javax.swing.JOptionPane
+import javax.swing.JTextArea
 
 /** Window to display the Acumen manual, invoked from the Help menu. */
 object ManualBrowser extends Frame {
@@ -15,8 +20,24 @@ object ManualBrowser extends Frame {
   title = "User Guide and Reference Manual"
   
   val jep = new JEditorPane
+  jep.setEditorKit(JEditorPane createEditorKitForContentType "text/html")
   jep.setEditable(false)
   jep.setBorder(new EmptyBorder(5, 5, 5, 0))
+  
+  jep.addHyperlinkListener(new HyperlinkListener {
+    def hyperlinkUpdate(e: HyperlinkEvent) {
+      if(e.getEventType == HyperlinkEvent.EventType.ACTIVATED)
+        if(Desktop isDesktopSupported)
+          Desktop.getDesktop browse (e.getURL.toURI)
+        else {
+          val message = new JTextArea("A newer version of Java (6 or later) is required to open links.\n" + 
+            "Please try the online version of this manual at: http://bit.ly/Acumen-manual-2014.")
+          message setEditable false
+          message setOpaque false
+          JOptionPane.showMessageDialog(ManualBrowser.peer, message, "Can not open link", JOptionPane.ERROR_MESSAGE)
+        }
+    }
+  })
 
   jep.setPage(getClass getResource "manual.html")
   jep.getDocument.asInstanceOf[HTMLDocument].getStyleSheet.addRule("body { font-family: sans-serif; }")
