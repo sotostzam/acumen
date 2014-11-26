0b847202bc26d60c4ab1f0bd4145880cd6ceb321
Adam Duracz <adam@duracz.net>
2014-11-26 14:56:13 +0100

    Disable tests for models with duplicate assignments.
    Pending a fix for these models.

--- src/test/scala/acumen/interpreters/reference2014/Reference2014SemanticsTest.scala
+++ src/test/scala/acumen/interpreters/reference2014/Reference2014SemanticsTest.scala
@@ -7,5 +7,12 @@ import acumen.interpreters.semanticstest._
 class Reference2014SemanticsTest extends Traditional2014Tests {
 
   def semantics = SemanticsImpl.Ref2014
+  
+  override def examplesSkip(f: String) = {
+    super.examplesSkip(f) ||
+    f.endsWith("01_CPS_Course/07_Game_Theory/01_The_Independence_Pattern_example.acm") ||
+    f.endsWith("03_Projects/02_Jawad/02_ADAS_RTL_Rear_END.acm") ||
+    f.endsWith("02_Jawad/04_ADAS_REAR_END_novisual.acm")
+  }
 
 }
