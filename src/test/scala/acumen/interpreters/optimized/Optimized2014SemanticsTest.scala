7a2232a34ffea6a5ff78beda65d6195c282c2a33
Adam Duracz <adam@duracz.net>
2014-11-27 12:47:44 +0100

    Re-enable test for game theory model.
    01_CPS_Course/07_Game_Theory/01_The_Independence_Pattern_example.acm
    
    This relates to ACUMEN-499.

--- src/test/scala/acumen/interpreters/optimized/Optimized2014SemanticsTest.scala
+++ src/test/scala/acumen/interpreters/optimized/Optimized2014SemanticsTest.scala
@@ -10,7 +10,6 @@ class Optimized2014SemanticsTest extends Traditional2014Tests {
 
   override def examplesSkip(f: String) = {
     super.examplesSkip(f) ||
-    f.endsWith("01_CPS_Course/07_Game_Theory/01_The_Independence_Pattern_example.acm") ||
     f.endsWith("03_Projects/02_Jawad/02_ADAS_RTL_Rear_END.acm") ||
     f.endsWith("02_Jawad/04_ADAS_REAR_END_novisual.acm")
   }
