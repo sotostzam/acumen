8ef7d1102f92b0e5b8ae7b763511ff5d9d025dae
Adam Duracz <adam@duracz.net>
2015-02-22 18:54:22 +0100

    Ignore Zeno models in Ref2015 regression test.
    These models stall when executed using the 2015 Reference
    semantics.

--- src/test/scala/acumen/interpreters/reference2015/Reference2015SemanticsTest.scala
+++ src/test/scala/acumen/interpreters/reference2015/Reference2015SemanticsTest.scala
@@ -10,8 +10,13 @@ class Reference2015SemanticsTest extends Traditional2015Tests {
   
   override def examplesSkip(f: String) = {
     super.examplesSkip(f) ||
-    f.endsWith("03_Projects/02_Jawad/02_ADAS_RTL_Rear_END.acm") ||
-    f.endsWith("02_Jawad/04_ADAS_REAR_END_novisual.acm")
+      List( "03_Projects/02_Jawad/02_ADAS_RTL_Rear_END.acm"
+          , "02_Jawad/04_ADAS_REAR_END_novisual.acm"
+          , "09_Ha-extract/05_two-tanks-extracted-w-else.acm" // These are Zeno models,
+          , "09_Ha-extract/06_two-tanks-extracted.acm"        // and will stall at t = 2.0 
+          , "09_Ha-extract/07_two-tanks.acm"                  // Zeno point when simulated
+          , "09_Ha-extract/08_two-two-tanks-helper.acm"       // using 2015 semantics
+          ).exists(f endsWith _)
   }
 
 }
