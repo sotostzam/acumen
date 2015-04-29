c2bf3d898524bcde4364f819fccd9cf3552c9237
Adam Duracz <adam@duracz.net>
2015-04-29 17:10:25 +0200

    Add some basic Dif arithmetic properties.

--- src/test/scala/acumen/ADTest.scala
+++ src/test/scala/acumen/ADTest.scala
@@ -13,12 +13,51 @@ import AD._
 
 object ADTest extends Properties("AD") {
 
-  property("Dif[Interval].sin") = forAll { (d: Dif[Interval]) =>
-    d.sin(0) == d(0).sin
+  /* Utilities */
+  
+  val evDoubleDifIsNum = implicitly[Num[Dif[Double]]]
+  
+  def liftInt(i: Int) = evDoubleDifIsNum.lift(i)
+  val one = evDoubleDifIsNum.one
+  
+  implicit class DoubleOps(l: Double) {
+    def ~=(r: Double): Boolean = 
+      Math.abs(l - r) <= Math.abs(Math.min(Math.abs(l), Math.abs(r))) / 1000
   }
   
-  property("Dif[Interval].cos") = forAll { (d: Dif[Interval]) =>
-    d.cos(0) == d(0).cos
+  implicit class DoubleDifOps(l: Dif[Double]) {
+     def ~=(r: Dif[Double]): Boolean =
+       (l.coeff zip r.coeff).forall{ case (lc, rc) => 
+       if (!(lc ~= rc))
+         sys.error("NO: " + (lc,rc) + "\nNO:\n" + l + "\n" + r)
+         else
+       lc ~= rc }
+  }
+  
+  /* Properties */
+
+  property("Dif[Double].add: x+x ~= 2*x") = forAll(genSmallDoubleDif) { (x: Dif[Double]) =>
+    (x + x) ~= (liftInt(2) * x)
+  }
+  
+  property("Dif[Double].add: 1+x > x") = forAll(genSmallDoubleDif) { (x: Dif[Double]) =>
+    (one + x) > x
+  }
+  
+  property("Dif[Double].mul: x*x >= x") = forAll { (x: Dif[Double]) =>
+    (x * x) >= x  
+  }  
+
+  property("Dif[Double].mul: x*1 ~= x && 1*x ~= x") = forAll { (x: Dif[Double]) =>
+    ((x * one) ~= x) && ((one * x) ~= x) 
+  }  
+  
+  property("Dif[Double].div: x/1 ~= x") = forAll { (x: Dif[Double]) =>
+    (x / one) ~= x
+  }
+
+  property("Dif[Double].div: x/x ~= 1") = forAll { (x: Dif[Double]) =>
+    (x(0) != 0d) ==> ((x / x) ~= one)  
   }
 
   property("lower is inverse of lift") = forAll { (st: CStore) =>
@@ -32,6 +71,13 @@ object ADTest extends Properties("AD") {
       coeffs <- listOfN(10, genN)
     } yield Dif(coeffs.to[Vector])
   
+  def genSmallDoubleDif: Gen[Dif[Double]] = genBoundedDoubleDif(1000000)
+    
+  def genBoundedDoubleDif(magnitude: Double): Gen[Dif[Double]] = {
+    val abs = Math.abs(magnitude)       
+    genDif(choose(-abs, abs))
+  }
+    
   implicit def arbitraryIntDif: Arbitrary[Dif[Int]] =
     Arbitrary(genDif(arbitrary[Int]))
   implicit def arbitraryDoubleDif: Arbitrary[Dif[Double]] =
