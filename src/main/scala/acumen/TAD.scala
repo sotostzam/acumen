244fb3d9da50fb38f588ac3ccab0bac36d906fb7
Adam Duracz <adam@duracz.net>
2015-09-08 16:55:47 +0200

    Fix bug introduced when porting sqrt to Streams.
    This relates to ACUMEN-620.

--- src/main/scala/acumen/TAD.scala
+++ src/main/scala/acumen/TAD.scala
@@ -86,7 +86,7 @@ object TAD extends App {
             else if (k == k0d2) x0.sqrt // the first non-zero coefficient of the result
             else {                      // possibly  non-zero coefficients k0/2 + 1 .. n - 1
               val kPk0d2 = k + k0d2
-              val kEnd = kPk0d2 + (kPk0d2 % 2) - 2
+              val kEnd = (kPk0d2 + (kPk0d2 % 2) - 2) / 2
               (x(kPk0d2) - evVIsIntegral.fromInt(2) * ((k0d2 + 1 to kEnd).foldLeft(zeroOfV) { 
                 case (sum, i) => sum + coeff(i) * coeff(kPk0d2 - i)  
               }) + (if (kPk0d2 % 2 == 0) - coeff(kPk0d2 / 2).square else zeroOfV )) / (evVIsIntegral.fromInt(2) * coeff(k0d2)) 
